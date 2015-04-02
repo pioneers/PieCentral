759b966607f1488c0d2e4d8943ed1f5127f120a0
Allen Li <allen@allen.li>
2015-04-02 00:57:00 -0700

    Implement multiprocessing version

--- griff/bravo.js
+++ griff/bravo.js
@@ -1,22 +1,25 @@
 var zmq = require('zmq');
-var PORT = '12355';
+var SEND_PORT = '12356';
+var RECV_PORT = '12355'
 
 // socket to talk to server
-var sock = zmq.socket('pair');
+var send_sock = zmq.socket('pair');
+var recv_sock = zmq.socket('pair');
 
-sock.connect('tcp://localhost:' + PORT);
+send_sock.connect('tcp://localhost:' + SEND_PORT);
+recv_sock.connect('tcp://localhost:' + RECV_PORT);
 
-sock.send_json = function send_json(obj) {
+send_sock.send_json = function send_json(obj) {
   return this.send(JSON.stringify(obj))
 }
 
-sock.on('message', function (buffer) {
+recv_sock.on('message', function (buffer) {
   msg = JSON.parse(buffer.toString()); // parse the buffer
   console.log('Got message:');
   console.log(msg);
   msg.resp = 'awesome response';
   console.log('Sending back:');
   console.log(msg);
-  sock.send_json(msg);
+  send_sock.send_json(msg);
 });
 
--- griff/golf.py
+++ griff/golf.py
@@ -1,27 +1,61 @@
 import zmq
-import zmq.eventloop.zmqstream
-import sys
-import time
+from multiprocessing import Process, Queue
+from Queue import Empty
 
-PORT = '12355'
+# Sender process.
+def sender(port, send_queue):
+    context = zmq.Context()
+    socket = context.socket(zmq.PAIR)
+    socket.bind("tcp://127.0.0.1:%d" % port)
+    while True:
+        msg = send_queue.get()
+        socket.send_json(msg)
 
-context = zmq.Context()
+# Receiver process.
+def receiver(port, recv_queue):
+    context = zmq.Context()
+    socket = context.socket(zmq.PAIR)
+    socket.bind("tcp://127.0.0.1:%d" % port)
+    while True:
+        msg = socket.recv_json()
+        recv_queue.put(msg)
 
-socket = context.socket(zmq.PAIR)
-socket.bind("tcp://127.0.0.1:%s" % PORT)
-stream = zmq.eventloop.zmqstream.ZMQStream(socket)
+send_queue = None
+recv_queue = None
 
-def receive_function():
-    msg = socket.recv_json()
-    print msg
+# Initializer function. Must be called first.
+def init(send_port=12355, recv_port=12356):
+    global send_queue
+    global recv_queue
 
-stream.on_recv(receive_function)
+    send_queue = Queue()
+    recv_queue = Queue()
 
-x = 0
-while True:
-    socket.send_json({'msg': 'server message ' + str(x)})
-    x += 1
-    msg = socket.recv_json()
-    print msg
-    print 'Sending message...'
-    time.sleep(1)
+    send_process = Process(target=sender, args=(send_port, send_queue))
+    recv_process = Process(target=receiver, args=(recv_port, recv_queue))
+    send_process.start()
+    recv_process.start()
+
+# Sends a message. Not blocking.
+def send(msg):
+    # todo make non-blocking
+    send_queue.put_nowait(msg)
+
+# Receives a message, or None if there is no current message.
+def recv():
+    # todo make non-blocking
+    try:
+        return recv_queue.get_nowait()
+    except Empty:
+        return None
+
+if __name__ == '__main__':
+    import time
+    init()
+    x = 0
+    while True:
+        x += 1
+        send({'msg': 'server message ' + str(x)})
+        print recv()
+        time.sleep(1)
+

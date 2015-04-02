8505cd149c1eba384c1865589f61e7b48cd23d1a
Allen Li <allen@allen.li>
2015-04-02 13:19:43 -0700

    kinda works

--- griff/socket_bridge/app.js
+++ griff/socket_bridge/app.js
@@ -1,8 +1,18 @@
 PORT = 12345
 var io = require('socket.io')(PORT);
+var ansible = require('./ansible');
 
 io.on('connection', function (socket) {
+  console.log('Socket.io connected.');
+  ansible.on('message', function (data) {
+    socket.send(data);
+    console.log('Passing data to client:');
+    console.log(data);
+  });
+
   socket.on('message', function (data) {
+    ansible.send(data);
+    console.log('Passing data to runtime:');
     console.log(data);
   });
 });

fa3e390b6f96053cefef91f5b743849682c1d179
Allen Li <allen@allen.li>
2015-06-25 21:50:42 -0700

    delete files from the old world

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,1 @@
-set -ev
-./node_modules/karma/bin/karma start --browsers NodeWebkitTravis \
-test/karma.conf.js \
---single-run --log-level=debug
 exit
--- scripts/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf node_modules bower_components .git
-npm install
-./node_modules/.bin/bower install

5a677eb0a2b56e0f849c1deada69acfa2a85acc5
Kyungna Kim <kyungna.k@berkeley.edu>
2015-07-24 22:51:00 -0700

    fixed up with recced changes

--- LimitSwitch.ino
+++ LimitSwitch.ino
@@ -1,49 +1,49 @@
 #include "hibike_message.h"
 
-int inPin = 3;
-int ledPin = 13;
+#define IN_PIN = 3;
+#define LED_PIN = 13;
 int data;
-unsigned long ms;
-unsigned long ms_compare;
-uint32_t time_passed;
+unsigned long long ms;
+unsigned long long ms_compare;
+uint64_t time_passed;
 hibike_message_t* m;
 int subscribed;
-
-uint16_t len = 0xFFFF;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(inPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(IN_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
   ms = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  data = digitalRead(inPin);
+  data = digitalRead(IN_PIN);
   ms_compare = millis();
   m = recieve_message();
   // message parsing
+  if (subscribed) {
+    ms = millis();
+    time_passed = ms - ms_compare;
+      if (time_passed > update_delay_time) {
+        // find what the inputs to the following function are
+        send_subscription_update(SENSOR_TYPE.LIMIT_SWITCH, sizeof(data), &data);
+      }
+    }
   if (m) {
     switch (m->message_id) {
-    case SUBSCRIPTION_REQUEST:
+    case HIBIKE_MESSAGE.SUBSCRIPTION_REQUEST:
+      HIBIKE_MESSAGE.SUBSCRIPTION_RESPONSE = SUCCESS;
       send_subscription_response(0x00);
       subscribed = 1;
       break;
-    case SUBSCRIPTION_SENSOR_UPDATE:
-      if (subscribed) {
-        ms = millis();
-        time_passed = (uint32_t) (ms - ms_compare);
-        if (time_passed > update_delay_time) {
-          // find what the inputs to the following function are
-          send_subscription_update(0x00, len, uint8_t *data_ptr);
-        }
-      }
+    case HIBIKE_MESSAGE.SUBSCRIPTION_SENSOR_UPDATE:
       break;
     //error case
-    case 0xFF:
-      send_error(OxFF);
+    case HIBIKE_MESSAGE.ERROR:
+      Serial.print(HIBIKE_MESSAGE.ERROR)
+      send_error(HIBIKE_MESSAGE.ERROR);
       break;
     default: break;
     }

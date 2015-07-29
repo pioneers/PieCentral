b9aa378a570572fb547640f9a21c9fed4bdc04d9
Vincent Donato <vincentdonato@pioneers.berkeley.edu>
2015-07-29 00:57:01 -0700

    Start work rewriting hibike.
    Now that we have more reason to suspect that the hibike protocol works
    and is somewhat viable (a bit more stress testing since last time has
    shown that we can send messages at ~100Hz with no problems), it's time
    to start making the code NOT horrendous!
    
    Start (more or less) rewriting the entire protocol from scratch to be
    object oriented, as well as actually in C++, on the Arduino side. The
    goal is to make the python implementation on the Beaglebone end as
    similar as possible for ease of maintenance. Additionally, new staff
    will be able to simple shift to a different syntax instead of having to
    pick up a new programming paradigm.

--- lib/hibike_message.cpp
+++ lib/hibike_message.cpp
@@ -1,192 +1,79 @@
 #include "hibike_message.h"
 
-//
-// The controller_id of this hibike endpoint.
-//
-uint8_t controller_id;
+//// CLASS METHOD IMPLEMENTATIONS /////////////////////////////////////////////
 
-//
-// helper function declarations. see function definitions for
-// more documentation
-//
-void send_message(hibike_message_t*);
-uint8_t calculate_checksum(hibike_message_t*);
-
-//
-// the rx buffer. max message limit of
-// 256 bytes was chosen more or less
-// arbitrarily. we'll realistically never
-// need to send such a large message
-//
-uint8_t rx_buffer[256] = {0};
-
-//
-// the amount of time to wait in between sending SUBSCRIPTION_SENSOR_UPDATEs
-//
-uint32_t delay_time = 0;
-
-
-//
-// Initializes this hibike endpoint, setting its controller_id to
-// my_controller_id. Aside from this, the function is really just a
-// glorified wrapper for Arduino serial setup.
-//
-void hibike_init(uint8_t my_controller_id) {
-  controller_id = my_controller_id;
-  Serial.begin(57600); // baud rate arbitrarily chosen for now. may change
+//// FUN WITH CHECKSUMS ///////////////////////////////////////////////
+uint8_t HibikeMessage::getChecksum() {
+  if (!checksumCalculated) {
+    checksum = calculateChecksum()
+  }
+  return checksum;
 }
 
-//
-// Sends a subscription response to the beaglebone controller.
-//
-void send_subscription_response(uint8_t error_code) {
-  hibike_message_t m;
-  m.message_id = SUBSCRIPTION_RESPONSE;
-  m.controller_id = controller_id;
-  m.payload.error_code = error_code;
-  send_message(&m);
+void SubscriptionRequest::calculateChecksum() {
+  uint8_t cs = 0;
+  cs ^= messageId;
+  cs ^= controllerId;
+  cs ^= subscriptionDelay & 0xFF;
+  cs ^= (subscriptionDelay >> 8) & 0xFF;
+  cs ^= (subscriptionDelay >> 16) & 0xFF;
+  cs ^= (subscriptionDelay >> 24) & 0xFF;
+  checksum = cs;
+  checksumCalculated = true;
 }
 
-//
-// Sends a subscription sensor update to the beaglebone controller.
-//
-void send_subscription_sensor_update(uint8_t sensor_type_id, uint16_t len, uint8_t *data) {
-  hibike_message_t m;
-  m.message_id = SUBSCRIPTION_SENSOR_UPDATE;
-  m.controller_id = controller_id;
-  m.payload.sensor_data.sensor_type_id = sensor_type_id;
-  m.payload.sensor_data.sensor_reading_length = len;
-  m.payload.sensor_data.data = data;
-  send_message(&m);
+void SubscriptionResponse::calculateChecksum() {
+  uint8_t cs = 0;
+  cs ^= messageId;
+  cs ^= controllerId;
+  cs ^= errorCode;
+  checksum = cs;
+  checksumCalculated = true;
 }
 
-//
-// Sends a hibike error message with the given error code
-// to the other hibike endpoint.
-//
-void send_error(uint8_t error_code) {
-  hibike_message_t m;
-  m.message_id = ERROR;
-  m.controller_id = controller_id;
-  m.payload.error_code = error_code;
-  send_message(&m);
+void SubscriptionSensorUpdate::calculateChecksum() {
+  uint8_t cs = 0;
+  cs ^= messageId;
+  cs ^= controllerId;
+  cs ^= sensorTypeId;
+  cs ^= sensorReadingLength & 0xFF
+  cs ^= (sensorReadingLength >> 8) & 0xFF
+  for (int i = 0; i < sensorReadingLength; i++) {
+    cs ^= *(dataPtr+i) & 0xFF;
+  }
+  checksum = cs;
+  checksumCalculated = true;
+}
+
+void Error::calculateChecksum() {
+  uint8_t cs = 0;
+  cs ^= messageId;
+  cs ^= controllerId;
+  cs ^= errorCode
+  checksum = cs;
+  checksumCalculated = true;
+}
+
+//// MESSAGE SENDING //////////////////////////////////////////////////
+void SubscriptionRequest::send() {
+}
+void SubscriptionResponse::send() {
+}
+void SubscriptionSensorUpdate::send() {
+}
+void Error::send() {
 }
 
 //
 // Receives an incoming hibike message.
-// Note that the pointer returned points to the rx_buffer.
 // Returns a null pointer if no data is on the serial port.
 //
 // Note that Arduino's Serial.readBytes function automatically
 // waits until either the given number of bytes are successfully
 // read or a timeout occurs (defaults to 1000ms).
 //
-hibike_message_t* receive_message() {
+HibikeMessage* receiveHibikeMessage() {
   if (!Serial.available()) {
     return NULL;
   }
-  hibike_message_t *m = (hibike_message_t*) rx_buffer;
-  Serial.readBytes(&m->message_id, sizeof(uint8_t));
-  Serial.readBytes(&m->controller_id, sizeof(uint8_t));
-  switch(m->message_id) {
-    //
-    // these message types are either currently unsupported
-    // or should never be sent to this type of endpoint. In
-    // either case, send back an error.
-    //
-    case SUBSCRIPTION_RESPONSE:
-    case SUBSCRIPTION_SENSOR_UPDATE:
-    case SENSOR_UPDATE:
-    case SENSOR_UPDATE_REQUEST:
-      send_error(INVALID_MESSAGE_TYPE);
-      break;
-    case SUBSCRIPTION_REQUEST:
-      Serial.readBytes((uint8_t *)&m->payload.delay, (size_t) sizeof(uint32_t));
-      break;
-    case ERROR:
-      Serial.readBytes(&m->payload.error_code, sizeof(uint8_t));
-      break;
-  }
-  Serial.readBytes(&m->checksum, sizeof(uint8_t));
-  uint8_t checksum = calculate_checksum(m);
-  if (!(checksum^m->checksum)) {
-    //TODO: implement message retries
-    send_error(CHECKSUM_MISMATCH);
-  }
-}
-
-//
-// Calculates and sets the checksum for the given hibike_message
-//
-uint8_t calculate_checksum(hibike_message_t *m) {
-  //
-  // (currently) unsupported message types
-  //
-  if (m->message_id == SENSOR_UPDATE ||
-      m->message_id == SENSOR_UPDATE_REQUEST) {
-    return 0;
-  }
-  uint8_t checksum = 0;
-  checksum ^= m->message_id;
-  checksum ^= m->controller_id;
-  switch(m->message_id) {
-    case SUBSCRIPTION_REQUEST:
-      checksum ^= m->payload.delay & 0xFF;
-      checksum ^= (m->payload.delay >> 8) & 0xFF;
-      checksum ^= (m->payload.delay >> 16) & 0xFF;
-      checksum ^= (m->payload.delay >> 24) & 0xFF;
-      break;
-    case SUBSCRIPTION_RESPONSE:
-    case ERROR:
-      checksum ^= m->payload.error_code;
-      break;
-    case SUBSCRIPTION_SENSOR_UPDATE:
-      checksum ^= m->payload.sensor_data.sensor_type_id;
-      checksum ^= m->payload.sensor_data.sensor_reading_length & 0xFF;
-      checksum ^= (m->payload.sensor_data.sensor_reading_length >> 8) & 0xFF;
-      uint8_t len = m->payload.sensor_data.sensor_reading_length;
-      uint8_t *data = m->payload.sensor_data.data;
-      for (int i = 0; i < len; i++) {
-        checksum ^= *(data+i);
-      }
-      break;
-  }
-  return checksum;
-}
-
-//
-// Sends the given hibike message over serial to the other
-// hibike endpoint. The standard use case of this will be to
-// send messages from arduino->beaglebone.
-//
-// We could definitely make calculate_checksum/send_message
-// more efficient by doing both at the same time, but I'm currently
-// too lazy to implement this.
-// TODO(vincent): get this optimization done eventually.
-void send_message(hibike_message_t *m) {
-  //
-  // unsupported message types and messages that
-  // should never be sent from this endpoint
-  //
-  if (m->message_id == SUBSCRIPTION_REQUEST ||
-      m->message_id == SENSOR_UPDATE ||
-      m->message_id == SENSOR_UPDATE_REQUEST) {
-    return;
-  }
-  m->checksum = calculate_checksum(m);
-  Serial.write(&m->message_id, sizeof(uint8_t));
-  Serial.write(&m->controller_id, sizeof(uint8_t));
-  switch(m->message_id) {
-    case SUBSCRIPTION_RESPONSE:
-    case ERROR:
-      Serial.write(&m->payload.error_code, sizeof(uint8_t));
-      break;
-    case SUBSCRIPTION_SENSOR_UPDATE:
-      Serial.write(&m->payload.sensor_data.sensor_type_id, (size_t) sizeof(uint8_t));
-      size_t len = m->payload.sensor_data.sensor_reading_length;
-      Serial.write((uint8_t *) &len, (size_t) sizeof(uint16_t));
-      Serial.write(m->payload.sensor_data.data, len);
-      break;
-  }
-  Serial.write(&m->checksum, sizeof(uint8_t));
 }
--- lib/hibike_message.h
+++ lib/hibike_message.h
@@ -3,90 +3,103 @@
 #include "Arduino.h"
 #include "assert.h"
 
-// see comment qqing about Arduino's lack of support for scoped enums.
-#define SUBSCRIPTION_REQUEST       0x00
-#define SUBSCRIPTION_RESPONSE      0x01
-#define SUBSCRIPTION_SENSOR_UPDATE 0x02
-#define SENSOR_UPDATE_REQUEST      0x03
-#define SENSOR_UPDATE              0x04
-#define ERROR                      0xFF
-#define SUCCESS                    0x00
-#define LIMIT_SWITCH               0x00
-#define LINE_FOLLOWER              0x01
-#define INVALID_MESSAGE_TYPE       0xFB
-#define MALFORMED_MESSAGE          0xFC
-#define INVALID_ARDUINO_ID         0xFD
-#define CHECKSUM_MISMATCH          0xFE
-
 /*
- * Arduino unfortunately has yet to support C++11 (and thus scoped enums)
- * instead, we're more or less forced to use the more ambiguous #define
- * statements above.
- *
-enum HIBIKE_MESSAGE {
-  SUBSCRIPTION_REQUEST,
-  SUBSCRIPTION_RESPONSE,
-  SUBSCRIPTION_SENSOR_UPDATE,
-  SENSOR_UPDATE_REQUEST,
-  SENSOR_UPDATE,
-  ERROR = 0xFF,
+ * qq. Arduino doesn't support C++11 currently, and thus we can't used scoped
+ * enums here. Note that the transition to scoped enums should be made as soon
+ * as it becomes possible
+ */
+// Message types
+#define SUBSCRIPTION_REQUEST        0x00
+#define SUBSCRIPTION_RESPONSE       0x01
+#define SUBSCRIPTION_SENSOR_UPDATE  0x02
+#define SENSOR_UPDATE_REQUEST       0x03
+#define SENSOR_UPDATE               0x04
+#define ERROR                       0xFF
+// Sensor types
+#define LIMIT_SWITCH                0x00
+#define LINE_FOLLOWER               0x01
+// Error codes
+#define SUCCESS                     0x00
+#define INVALID_MESSAGE_TYPE        0xFB
+#define MALFORMED_MESSAGE           0xFC
+#define INVALID_ARDUINO_ID          0xFD
+#define CHECKSUM_MISMATCH           0xFE
+
+
+// top level abstract HibikeMessage class
+class HibikeMessage
+{
+  private:
+    uint8_t messageId;
+    uint8_t controllerId;
+    uint8_t checksum;
+    bool checksumCalculated;
+    virtual void calculateChecksum() = 0;
+
+  public:
+    HibikeMessage(uint8_t mId, uint8_t cId):
+      messageId(mId), controllerId(cId), checksum(0), checksumCalculated(false) {}
+    // getter functions. We want to ensure that HibikeMessages cannot be further altered
+    // after construction, and thus we choose to have the class fields private and their
+    // values accessible using getters. Note that depending
+    uint8_t getMessageId() { return messageId; }
+    uint8_t getControllerId() { return controllerId; }
+    uint8_t getChecksum();
+    virtual void send() = 0;
 };
 
-enum SUBSCRIPTION_RESPONSE {
-  SUCCESS,
-  GENERIC_ERROR = 0xFF,
-};
+class SubscriptionRequest : public HibikeMessage
+{
+  private:
+    uint32_t subscriptionDelay;
 
-enum SENSOR_TYPE {
-  LIMIT_SWITCH,
-  LINE_FOLLOWER,
-};
+  public:
+    SubscriptionRequest(uint8_t cId, uint32_t sd):
+      HibikeMessage(SUBSCRIPTION_REQUEST, cId), subscriptionDelay(sd) {}
 
-enum ERROR {
-  INVALID_MESSAGE_TYPE = 0xFB,
-  MALFORMED_MESSAGE = 0xFC,
-  INVALID_ARDUINO_ID = 0xFD,
-  CHECKSUM_MISMATCH = 0xFE,
-  GENERIC_ERROR = 0xFF,
-};
-*/
+    uint32_t getSubscriptionDelay() { return subscriptionDelay; }
+}
 
-typedef struct {
-  uint8_t message_id;
-  uint8_t controller_id;
-  union {
-    uint32_t delay; // for use with MESSAGE_TYPE.SUBSCRIPTION_REQUEST
-    struct {
-      uint8_t sensor_type_id;
-      uint16_t sensor_reading_length;
-      uint8_t *data;
-    } sensor_data;
-    uint8_t error_code; // for use with MESSAGE_TYPE.SUBSCRIPTION_RESPONSE || MESSAGE_TYPE.ERROR
-  } payload;
-  uint8_t checksum;
-} hibike_message_t;
+class SubscriptionResponse : public HibikeMessage
+{
+  private:
+    uint8_t errorCode;
 
+  public:
+    SubscriptionResponse(uint8_t cId, uint8_t ec):
+      HibikeMessage(SUBSCRIPTION_RESPONSE, cId), errorCode(ec) {}
 
-//
-// the amount of time to wait in between SUBSCRIPTION_SENSOR_UPDATEs
-//
-extern uint32_t update_delay_time;
+    uint8_t getErrorCode() { return errorCode; }
+}
 
-void hibike_init(uint8_t);
+class SubscriptionSensorUpdate : public HibikeMessage
+{
+  private:
+    uint8_t sensorTypeId;
+    uint16_t sensorReadingLength;
+    uint8_t *dataPtr;
 
-//
-// TODO: The three commented out functions below would be nice to have but aren't
-//       strictly required. Finish implementing them eventually.
-//
-////////////////////////////// Message sending //////////////////////////////
-/* void send_subscription_request(uint32_t); */
-void send_subscription_response(uint8_t);
-void send_subscription_sensor_update(uint8_t, uint16_t, uint8_t*);
-/* void send_sensor_update_request(); */
-/* void send_sensor_update(uint8_t, uint16_t, *uint8_t); */
-void send_error(uint8_t);
+  public:
+    SubscriptionSensorUpdate(uint8_t cId, uint8_t sId, uint16_t srl, uint8_t *p):
+      HibikeMessage(SUBSCRIPTION_SENSOR_UPDATE, cId),
+      sensorTypeId(sId), sensorReadingLength(srl), dataPtr(p) {}
+
+    uint8_t getSensorTypeId() { return sensorTypeId; }
+    uint16_t getSensorReadingLength() { return sensorReadingLength; }
+    uint8_t* getPtrToData() { return dataPtr; }
+}
+
+class Error : public HibikeMessage
+{
+  private:
+    uint8_t errorCode;
+
+  public:
+    Error(uint8_t cId, uint8_t ec):
+      HibikeMessage(SUBSCRIPTION_RESPONSE, cId), errorCode(ec) {}
+    uint8_t getErrorCode() { return errorCode; }
+}
 
 ///////////////////////////// Message receiving /////////////////////////////
-hibike_message_t* receive_message();
-
+HibikeMessage* receiveHibikeMessage();
 #endif /* HIBIKE_H */

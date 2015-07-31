637a68ddd2688b35deebf15bf178b2193eab1bd0
Vincent Donato <vincentdonato@pioneers.berkeley.edu>
2015-07-31 01:06:02 -0700

    Start python implementation of HibikeMessage class and subclasses.
    Using python abstract base classes should probably be considered, but
    their behavior seems kinda strange compared to standard C++ notion of a
    base class so it may not be worth the confusion.
    
    Note that the __calculateChecksum() and send() methods need to be
    implemented for each HibikeMessage subclass as well as the (not class
    related) receiveHibikeMessage() function.

--- lib/hibike_message.py
+++ lib/hibike_message.py
@@ -13,13 +13,6 @@ class HibikeMessageType(Enum):
     Error                    = 0xFF
 
 """
-SubscriptionResponse status codes.
-"""
-class SubscriptionResponse(Enum):
-    Success      = 0x00
-    GenericError = 0xFF
-
-"""
 Sensor Type IDs.
 """
 class SensorType(Enum):
@@ -29,7 +22,7 @@ class SensorType(Enum):
 """
 More specific error codes.
 """
-class Error(Enum):
+class ErrorCode(Enum):
     InvalidMessageType = 0xFB
     MalformedMessage   = 0xFC
     InvalidArduinoId   = 0xFD
@@ -45,135 +38,80 @@ class HibikeMessageException(Exception):
 def getByte(value, index):
     return (value >> (index*8)) & 0xFF
 
-"""
-A Hibike message.
-Each hibike message, aside from the properties defined in the spec
-(found in ../README.md), each HibikeMessage also keeps track of the
-port it should be sent over, allowing it to essentially sent itself
-using HibikeMessage.send().
-"""
+
 class HibikeMessage:
-    # an integer containing the message_id of this message
-    # see the HibikeMessageType enum.
-    messageId = None
+    def __init__(self, messageId, controllerId, serial):
+        assert(isinstance(messageId, HibikeMessageType));
+        self.__messageId = messageId
+        self.__controllerId = controllerId
+        self.__checksum = 0
+        self.__serial = serial
+    def getMessageId():
+        return self.__messageId
+    def getControllerId():
+        return self.__controllerId
+    def getChecksum():
+        return self.__checksum
 
-    # an integer containing either the controller_id this message
-    # is being sent to (for an outgoing message), or the controller_id
-    # of the controller this message is coming from (for an incoming message)
-    controllerId = None
+class SubscriptionRequest(HibikeMessage):
+    def __init__(self, controllerId, subscriptionDelay, serial):
+        HibikeMessage.__init__(self, HibikeMessageType.SubscriptionRequest,
+                               controllerId, serial)
+        # assert subscriptionDelay is a uint32
+        self.__subscriptionDelay = subscriptionDelay
+        self.__calculateChecksum()
+    def getSubscriptionDelay():
+        return self.__subscriptionDelay
+    def __calculateChecksum():
+        pass
+    def send():
+        pass
 
-    # either an integer or dictionary containing the data for this message
-    payload = None
+class SubscriptionResponse(HibikeMessage):
+    def __init__(self, controllerId, serial):
+        HibikeMessage.__init__(self, HibikeMessageType.SubscriptionResponse,
+                               controllerId, serial)
+        self.__calculateChecksum()
+    def __calculateChecksum():
+        pass
+    def send():
+        pass
 
-    # an 8 bit integer calculated either right before sending a message or
-    # immediately upon receiving one.
-    checksum = None
+class SubscriptionSensorUpdate(HibikeMessage):
+    def __init__(self, controllerId, sensorTypeId, sensorReadingLength, data, serial):
+        HibikeMessage.__init__(self, HibikeMessageType.SubscriptionSensorUpdate,
+                               controllerId, serial)
+        assert(isinstance(sensorTypeId, SensorType))
+        # assert uint8
+        self.__sensorTypeId = sensorTypeId
+        # assert uint16
+        self.__sensorReadingLength = sensorReadingLength
+        # assert size in bytes is consistent with sensorReadingLength
+        self.__data = data
+        self.__calculateChecksum()
+    def getSensorTypeId():
+        return self.__sensorTypeId
+    def getSensorReadingLength():
+        return self.__sensorReadingLength
+    def getData():
+        return self.__data
+    def __calculateChecksum():
+        pass
+    def send():
+        pass
 
-    def __init__(self, messageId, controllerId, payload):
-        self.messageId = messageId
-        self.controllerId = controllerId
-        self.payload = payload
-        self.__verifyMessage()
+class Error(HibikeMessage):
+    def __init__(self, controllerId, errorCode):
+        HibikeMessage.__init__(self, HibikeMessageType.Error, controllerId, serial)
+        assert(isinstance(errorCode, ErrorCode))
+        self.__errorCode = errorCode
+        self.__calculateChecksum()
+    def getErrorCode():
+        return self.__errorCode
+    def __calculateChecksum():
+        pass
+    def send():
+        pass
 
-    """
-    Ensures that the message constructed appears to be sane.
-    """
-    def __verifyMessage(self):
-        assert isinstance(self.messageId, HibikeMessageType)
-        # TODO: add more checks/asserts to ensure that the values of
-        #       payload are within possible ranges.
-        if self.messageId is HibikeMessageType.SubscriptionRequest:
-            assert type(self.payload) is int
-            # verify that self.payload can fit in a 32 bit uint
-        elif self.messageId is HibikeMessageType.SubscriptionResponse:
-            assert isinstance(self.payload, SubscriptionResponse)
-        elif self.messageId is HibikeMessageType.SubscriptionSensorUpdate:
-            assert type(self.payload) is dict
-            # verify that self.payload contains sensorTypeId, sensorReadingLength,
-            # and data and that each field is sensible.
-        elif self.messageId is HibikeMessageType.Error:
-            assert isinstance(self.payload, Error)
-        else:
-            raise HibikeMessageException('Message type currently unsupported.')
-
-    def calculateChecksum(self):
-        checksum = 0
-        checksum ^= self.messageId.value
-        checksum ^= self.controllerId
-        if self.messageId == HibikeMessageType.SubscriptionRequest:
-            checksum ^= (self.payload & 0xFF) ^ (self.payload>>8 & 0xFF) ^ \
-                        (self.payload >>16 & 0xFF) ^ (self.payload >>24 & 0xFF)
-        elif self.messageId == HibikeMessageType.SensorUpdateRequest:
-            pass
-        elif self.messageId == HibikeMessageType.SubscriptionResponse or \
-             self.messageId == HibikeMessageType.Error:
-            checksum ^= self.payload.value & 0xFF
-        elif self.messageId == HibikeMessageType.SubscriptionSensorUpdate:
-            checksum ^= self.payload['sensorTypeId'].value & 0xFF
-            checksum ^= self.payload['sensorReadingLength'] & 0xFF
-            checksum ^= (self.payload['sensorReadingLength'] >> 8) & 0xFF
-            length = self.payload['sensorReadingLength']
-            data = self.payload['reading']
-            for i in range(length):
-                checksum ^= getByte(data, i)
-        else:
-            raise HibikeMessageExeption
-        return checksum
-
-"""
-Send HibikeMessage m over port
-"""
-def sendHibikeMessage(m, port):
-    # TODO: fix to be able to send every message type.
-    #       it's also definitely better to send the entire message
-    #       at once as opposed to marshalling and sending off
-    #       small parts
-    m.checksum = m.calculateChecksum()
-    port.write(struct.pack('<B', m.messageId.value))
-    port.write(struct.pack('<B', m.controllerId))
-    if m.messageId == HibikeMessageType.SubscriptionRequest:
-        port.write(struct.pack('<I', m.payload))
-    else:
-        port.write(struct.pack('<B', m.payload.value))
-    port.write(struct.pack('<B', m.checksum))
-
-"""
-Receives a Hibike Message from the given serial port.
-Returns None if no data is available on the serial port.
-"""
-def receiveHibikeMessage(port):
-    if port.inWaiting() == 0:
-        return None
-    payload = None
-    messageId = HibikeMessageType(struct.unpack('<B', port.read(1))[0])
-    controllerId = struct.unpack('<B', port.read(1))[0]
-    if messageId == HibikeMessageType.SubscriptionResponse:
-        payload = SubscriptionResponse(struct.unpack('<B', port.read(1))[0])
-    elif messageId == HibikeMessageType.Error:
-        payload = Error(struct.unpack('<B', port.read(1))[0])
-    elif messageId == HibikeMessageType.SubscriptionSensorUpdate or \
-         messageId == HibikeMessageType.SensorUpdate:
-        payload = {}
-        payload['sensorTypeId'] = SensorType(struct.unpack('<B', port.read(1))[0])
-        payload['sensorReadingLength'] = struct.unpack('<H', port.read(2))[0]
-        # figure out what to do with the still serialized data later
-        # currently kludged so that limit switch stuff works
-        # TODO: smarter unpacking that works for sensors of any type
-        payload['reading'] = struct.unpack('<B', port.read(payload['sensorReadingLength']))[0]
-    checksum = struct.unpack('<B', port.read(1))[0]
-    message = HibikeMessage(messageId, controllerId, payload, port)
-    if message.calculateChecksum() != checksum:
-        errorMessage = HibikeMessage(HibikeMessageType.Error, controllerId, Error.ChecksumMismatch, port)
-        errorMessage.sendMessage()
-        raise HibikeMessageExeption("incorrect checksum")
-    return message
-
-"""
-Sends a subscriptionSensorRequest that requests new sensor
-data every delay ms to controllerId over port.
-"""
-def sendSubscriptionRequest(delay, controllerId, port):
-    message = HibikeMessage(HibikeMessageType.SubscriptionRequest,
-                            controllerId, delay, port)
-    sendHibikeMessage(message, port);
-
+def receiveHibikeMessage():
+    pass

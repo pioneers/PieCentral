a2acd4fa922af57aeeaddb5918fbc0e7bca4fb3e
Allen Li <allen@allen.li>
2015-08-02 22:29:52 -0700

    move MotorTester outside

--- app/client/dashboard/components/__tests__/MotorTester-test.js
+++ app/client/dashboard/components/__tests__/MotorTester-test.js
@@ -4,7 +4,7 @@ describe('MotorTester', function () {
   it('generates fake data', function () {
     var React = require('react/addons');
     var MotorTester = require('../MotorTester.js');
-    var RobotActions = require('../../../actions/RobotActions');
+    var RobotActions = require('../../actions/RobotActions');
     var TestUtils = React.addons.TestUtils;
 
     var tester = TestUtils.renderIntoDocument(

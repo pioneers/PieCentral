c53c63eeac9e1a8b4667758f59feb35ddfb6be50
Allen Li <allen@allen.li>
2015-08-08 10:53:18 -0700

    implement FCPL

--- app/client/dashboard/components/FinalCompPeripheralList.js
+++ app/client/dashboard/components/FinalCompPeripheralList.js
@@ -4,11 +4,30 @@
 
 import React from 'react';
 import PeripheralList from './PeripheralList';
+import Peripheral from './PeripheralList';
+import RemoteRobotStore from '../stores/RemoteRobotStore';
+import _ from 'lodash';
 
 var FinalCompPeripheralList = React.createClass({
+  getInitialState() {
+    return {
+      peripherals: []
+    };
+  },
+  onChange() {
+    this.setState({peripherals: RemoteRobotStore.getPeripherals()});
+  },
+  componentDidMount() {
+    RemoteRobotStore.on('change', this.onChange);
+    this.onChange();
+  },
+  componentWillUnmount() {
+    RemoteRobotStore.removeListener('change', this.onChange);
+  },
   render() {
     return (
       <PeripheralList header='Connected Components'>
+        {_.map(this.state.peripherals, (peripheral) => <Peripheral key={peripheral.id} {...peripheral}/>)}
       </PeripheralList>
     );
   }

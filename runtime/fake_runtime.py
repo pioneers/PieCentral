f3ff8c5fe6664acfcb22bc957dbf5269b6d3fbe6
jchen0022 <jchen0022@gmail.com>
2015-11-05 04:09:23 +0000

    updated fake_runtime.py

--- runtime/fake_runtime.py
+++ runtime/fake_runtime.py
@@ -12,3 +12,27 @@ while True:
             'value': random.randint(0, 100)
         }
     })
+    ansible.send_message('UPDATE_BATTERY', {
+        'battery': {
+            'value': random.randint(0,100) 
+        }
+    })
+    ansible.send_message('UPDATE_STATUS', {
+        'status': {
+            'value': random.randint(0,1)
+        }
+    })
+    ansible.send_message('UPDATE_PERIPHERAL', {
+        'peripheral': {
+            'id': 1235,
+            'peripheralType': 'MOTOR_SCALAR',
+            'value': random.randint(0, 100)
+        }
+    })
+    ansible.send_message('UPDATE_PERIPHERAL', {
+        'peripheral': {
+            'id': 1236,
+            'peripheralType': 'SENSOR_BOOLEAN',
+            'value': random.randint(0, 1)
+        }
+    })

826b17d608b964fa7d791fa14e354a2cc03a3a21
Tim Foster <fostertim4@gmail.com>
2015-11-07 13:38:38 -0800

    restructured ansible.py to be a class.

--- runtime/ansible.py
+++ runtime/ansible.py
@@ -20,10 +20,14 @@ class AMessage(object):
         }
 
 # Sender process.
-def sender(port, send_queue):
+def sender(port, send_queue, do_bind=True):
     context = zmq.Context()
     socket = context.socket(zmq.PAIR)
-    socket.bind("tcp://127.0.0.1:%d" % port)
+    if do_bind:
+       f=socket.bind
+    else:
+       f = socket.connect
+    f("tcp://127.0.0.1:%d" % port)
     while True:
         msg = send_queue.get()
         if isinstance(msg, AMessage):
@@ -31,50 +35,67 @@ def sender(port, send_queue):
         socket.send_json(msg)
 
 # Receiver process.
-def receiver(port, recv_queue):
+def receiver(port, recv_queue, do_bind=True):
     context = zmq.Context()
     socket = context.socket(zmq.PAIR)
-    socket.bind("tcp://127.0.0.1:%d" % port)
+    if do_bind:
+        f = socket.bind
+    else:
+        f=socket.connect
+    f("tcp://127.0.0.1:%d" % port)
     while True:
         msg = socket.recv()
         parsed = yaml.load(msg)
         recv_queue.put(parsed)
 
+
+class Ansible(object):
 # Doesn't do anything.
-def init():
-    pass
+    def __init__(self, role):
+        # Intialize on module import
+        if (role == 'dawn'):
+            self.send_port = 12355
+            self.recv_port = 12356
+        elif (role == 'runtime'):
+            self.send_port = 12357
+            self.recv_port = 12358
+        elif (role == 'student_code'):
+            self.send_port = 12358
+            self.recv_port = 12357
+        else:
+            raise ValueError()
 
-# DON'T USE THIS UNLESS YOU KNOW WHAT YOU'RE DOING
-# Low level message sending. For high level messaging, use send_msg.
-def send(msg):
-    send_queue.put_nowait(msg)
+        self.send_queue = Queue()
+        self.recv_queue = Queue()
 
-# Use this one instead of send
-def send_message(msg_type, content):
-    send({
-        'header': {'msg_type': msg_type},
-        'content': content
-    })
+        self.send_process = Process(target=sender, args=(self.send_port, self.send_queue,
+                                                         role != 'student_code'))
+        self.recv_process = Process(target=receiver, args=(self.recv_port, self.recv_queue,
+                                                           role != 'student_code'))
+        self.send_process.start()
+        self.recv_process.start()
 
-# Receives a message, or None if there is no current message.
-# If block is True, blocks.
-def recv(block=False):
-    if block:
-        return recv_queue.get()
-    else:
-        try:
-            return recv_queue.get_nowait()
-        except Empty:
-            return None
 
-# Intialize on module import
-send_port = 12355
-recv_port = 12356
+    # DON'T USE THIS UNLESS YOU KNOW WHAT YOU'RE DOING
+    # Low level message sending. For high level messaging, use send_msg.
+    def send(self, msg):
+        self.send_queue.put_nowait(msg)
 
-send_queue = Queue()
-recv_queue = Queue()
+    # Use this one instead of send
+    def send_message(self, msg_type, content):
+        self.send({
+            'header': {'msg_type': msg_type},
+            'content': content
+        })
 
-send_process = Process(target=sender, args=(send_port, send_queue))
-recv_process = Process(target=receiver, args=(recv_port, recv_queue))
-send_process.start()
-recv_process.start()
+    # Receives a message, or None if there is no current message.
+    # If block is True, blocks.
+    def recv(self, block=False):
+        if block:
+            return self.recv_queue.get()
+        else:
+            try:
+                return self.recv_queue.get_nowait()
+            except Empty:
+                return None
+

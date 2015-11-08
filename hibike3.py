a9a82f2e6ca4619481e0816772cc3aa5af100367
LIZA <etremsina@berkeley.edu>
2015-11-07 16:00:05 -0800

    changing init

--- hibike3.py
+++ hibike3.py
@@ -14,8 +14,10 @@ from hibike_message import *
 class Hibike():
 	def __init__(self, context):
 		self.context = context
+		self.connections = []
 		self._portList = self._getPorts()
         self._enumerateSerialPorts()
+
         self.thread = self._spawnHibikeThread()
 
 
@@ -24,6 +26,30 @@ class Hibike():
         h_thread.daemon = True
         h_thread.start()
         return h_thread
+
+    #TODO: get rid of locking and change to fit changes!
+    def _enumerateSerialPorts(self):
+        serial_conns = {p: serial.Serial(p, 115200) for p in self._portList}
+        time.sleep(3)
+
+        for p in self._portList:
+            serial_conns[p].flushInput()
+            send_sub_request(0, serial_conns[p])
+        time.sleep(2)
+        for p in self._portList:
+            msg = read(serial_conns[p])
+            if msg == None or msg == -1:
+                print("read() failed for ping response.")
+                continue
+            if msg.getmessageID() != messageTypes["SubscriptionResponse"]:
+                pdb.set_trace()
+                print("wrong message back: "+str(msg))
+
+            res = struct.unpack("<HBQH", msg.getPayload())
+            uid = (res[0] << 72) | (res[1] << 64) | (res[2])
+            self._devices[uid] = getDeviceType(uid)
+            self._data[uid] = 0
+            self._connections[uid] = (p, serial_conns[p])
 
 
 class HibikeThread(threading.Thread):

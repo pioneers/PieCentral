da1eaca361e7d7e121f7d2efd0be91683de9558f
AravindK95 <aravind.kumaraguru@gmail.com>
2015-11-07 16:56:57 -0800

    added methods to DeviceContext.py

--- deviceContext.py
+++ deviceContext.py
@@ -1,14 +1,24 @@
 class DeviceContext():
-	def __init__(self):
+	def __init__(self, deviceParams):
 		#contextData = {uid: {field: (value, timestamp)}, delay, timestamp) } 
 		self.contextData = dict()
+		self.deviceParams = deviceParams
+		self.hibike = None
 
 	def getData(uid, field):
 
 	def getDelay(uid):
 
-	def subToDevices(deviceTupeles):
+	def subToDevices(self, deviceTuples):
+		for devTup in deviceTuples:
+			self.subDevice(uid, delay)
 
-	def subDevice(uid):
+	def subDevice(self, uid, delay):
+		assert self.hibike is not None, "DeviceContext needs a pointer to Hibike!"
+		self.hibike.sendSubRequest(uid, delay)
 
-	def writeValue(uid, field, value):
+	def writeValue(self, uid, param, value):
+		assert self.hibike is not None, "DeviceContext needs a pointer to Hibike!"
+		assert param in self.deviceParams[getDevice(uid)], "Invalid param for {}".format(getDevice(uid))
+		
+		self.hibike.sendDeviceUpdate(uid, field, value)

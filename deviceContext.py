1fbaa32b05f22f608163432de8d35d30ee1ec112
jarzofjam <jannyzhang@gmail.com>
2015-11-07 16:57:44 -0800

    Wrote getData and getDelay methods for DeviceContext class.

--- deviceContext.py
+++ deviceContext.py
@@ -1,14 +1,27 @@
 class DeviceContext():
 	def __init__(self):
-		#contextData = {uid: {field: (value, timestamp)}, delay, timestamp) } 
+		#contextData = {uid: ({param: (value, timestamp)}, delay, timestamp) }
 		self.contextData = dict()
 
-	def getData(uid, field):
+	def getData(self, uid, param):
+		if uid in self.contextData:
+			if param in self.contextData[uid]:
+				return self.contextData[uid][param]
+			else:
+				return "The parameter {0} does not exist for your specified device.".format(param)
+		else:
+			return "You have not specified a valid device. Check your UID."
 
-	def getDelay(uid):
 
-	def subToDevices(deviceTupeles):
+	def getDelay(self, uid):
+		if uid in self.contextData:
+			return (self.contextData[uid][1], self.contextData[uid][2])
+		else:
+			return "You have not specified a valid device. Check your UID."
 
-	def subDevice(uid):
 
-	def writeValue(uid, field, value):
+	def subToDevices(self, deviceTupeles):
+
+	def subDevice(self, uid):
+
+	def writeValue(self, uid, param, value):

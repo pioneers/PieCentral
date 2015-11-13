2eee024b804885c01b2df7b32f169af018df0747
Allan Zhou <allan.zhou@berkeley.edu>
2015-11-13 18:51:25 +0000

    Working motors

--- runtime/ansible.py
+++ runtime/ansible.py
@@ -1,8 +1,6 @@
-import zmq, yaml, signal, time, sys, os
-from multiprocessing import *
+import zmq, yaml
+from multiprocessing import Process, Queue
 from Queue import Empty
-
-
 
 class AMessage(object):
     """Convenience class for sending Ansible Messages
@@ -22,14 +20,10 @@ class AMessage(object):
         }
 
 # Sender process.
-def sender(port, send_queue, do_bind=True):
+def sender(port, send_queue):
     context = zmq.Context()
     socket = context.socket(zmq.PAIR)
-    if do_bind:
-       f=socket.bind
-    else:
-       f = socket.connect
-    f("tcp://127.0.0.1:%d" % port)
+    socket.bind("tcp://127.0.0.1:%d" % port)
     while True:
         msg = send_queue.get()
         if isinstance(msg, AMessage):
@@ -37,84 +31,50 @@ def sender(port, send_queue, do_bind=True):
         socket.send_json(msg)
 
 # Receiver process.
-def receiver(port, recv_queue, do_bind=True):
+def receiver(port, recv_queue):
     context = zmq.Context()
     socket = context.socket(zmq.PAIR)
-    if do_bind:
-        f = socket.bind
-    else:
-        f=socket.connect
-    f("tcp://127.0.0.1:%d" % port)
+    socket.bind("tcp://127.0.0.1:%d" % port)
     while True:
         msg = socket.recv()
         parsed = yaml.load(msg)
         recv_queue.put(parsed)
 
-
-class Ansible(object):
 # Doesn't do anything.
-    def __init__(self, role):
-        # Intialize on module import
-        if (role == 'dawn'):
-            self.send_port = 12355
-            self.recv_port = 12356
-        elif (role == 'runtime'): # used for hibike (getting motor/sensor values, gamepad)
-            self.send_port = 12357
-            self.recv_port = 12358
-        elif (role == 'student_code'): # only used for setting motor values and sending to Runtime
-            print('student code')
-            self.send_port = 12358
-            self.recv_port = 12357
-        else:
-            raise ValueError()
+def init():
+    pass
 
-        self.send_queue = Queue()
-        self.recv_queue = Queue()
+# DON'T USE THIS UNLESS YOU KNOW WHAT YOU'RE DOING
+# Low level message sending. For high level messaging, use send_msg.
+def send(msg):
+    send_queue.put_nowait(msg)
 
-        self.send_process = Process(target=sender, args=(self.send_port, self.send_queue,
-                                                         role != 'student_code'))
-        self.recv_process = Process(target=receiver, args=(self.recv_port, self.recv_queue,
-                                                           role != 'student_code'))
-        self.send_process.start()
-        self.recv_process.start()
+# Use this one instead of send
+def send_message(msg_type, content):
+    send({
+        'header': {'msg_type': msg_type},
+        'content': content
+    })
 
-    #signal handlers
-    def sigterm_handler(signal1, fram):
-        for p in active_children():
-            print(p.pid, "PID")
-            os.kill(p.pid, signal.SIGTERM)
-            print(p, 'Before')
-            #p.terminate()
-#            time.sleep(0.1)
-            print(p, 'After')
-#        sys.exit(0)
+# Receives a message, or None if there is no current message.
+# If block is True, blocks.
+def recv(block=False):
+    if block:
+        return recv_queue.get()
+    else:
+        try:
+            return recv_queue.get_nowait()
+        except Empty:
+            return None
 
-    def sigint_handler(signal, fram):
-        sys.exit(0)
+# Intialize on module import
+send_port = 12355
+recv_port = 12356
 
-    signal.signal(signal.SIGINT, sigint_handler)
-    signal.signal(signal.SIGTERM, sigterm_handler)
+send_queue = Queue()
+recv_queue = Queue()
 
-    # DON'T USE THIS UNLESS YOU KNOW WHAT YOU'RE DOING
-    # Low level message sending. For high level messaging, use send_msg.
-    def send(self, msg):
-        self.send_queue.put_nowait(msg)
-
-    # Use this one instead of send
-    def send_message(self, msg_type, content):
-        self.send({
-            'header': {'msg_type': msg_type},
-            'content': content
-        })
-
-    # Receives a message, or None if there is no current message.
-    # If block is True, blocks.
-    def recv(self, block=False):
-        if block:
-            return self.recv_queue.get()
-        else:
-            try:
-                return self.recv_queue.get_nowait()
-            except Empty:
-                return None
-
+send_process = Process(target=sender, args=(send_port, send_queue))
+recv_process = Process(target=receiver, args=(recv_port, recv_queue))
+send_process.start()
+recv_process.start()

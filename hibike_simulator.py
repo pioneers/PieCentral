08f1c22a2606faba3a5d6784a195ba20c49c398b
Ananth Kuchibhotla <ananthkuchibhotla@berkeley.edu>
2015-11-14 15:37:19 -0800

    Improved implementation of flipping data. Added documentation for all functions.

--- hibike_simulator.py
+++ hibike_simulator.py
@@ -1,6 +1,10 @@
 import time, random
 
+#weight of each time interval for flipping.
+#for example, there is a 5% chance the data will flip within 1 sec
 cutoffs = [0.05, 0.13, 0.15, 0.27, 0.5, 0.73, 0.85, 0.95, 0.99, 1.0]
+
+# Sensor type: (low value, high value, noise)
 sensor_values = {
     '0x0000': (0, 1, 0),
     '0x0001': (200, 800, 100),
@@ -24,11 +28,12 @@ class Hibike:
             '0x000800FFFFFFFFFFFFFFFF'
          ]
         #format Device Type (16) + Year (8) + ones (64)
-        self.subscribedto = {}
+        self.subscribedTo = {}
         self.sensors = []
 
 
     def getEnumeratedDevices(self):
+        """ Returns a list of tuples of UIDs and device types. """
 
         enum_devices = []
         for UID in self.UIDs:
@@ -36,71 +41,88 @@ class Hibike:
         return enum_devices
 
     def subscribeToDevices(self, deviceList):
+        """ deviceList - List of tuples of UIDs and delays. Creates a dictionary
+        storing UID, delay, time created (ms), previous data, whether data is
+        low and flip times (the next time the data should flip). Flip is when
+        data changes low to high or high to low.
+
+        """
+
         for UID, delay in deviceList:
-            if UID in self.UIDs and UID not in self.subscribedto:
+            if UID in self.UIDs and UID not in self.subscribedTo:
                 last_time = time.time()*1000
-                self.subscribedto[UID] = {'delay': delay, 'time': last_time, 'data': self.getRandomData(UID, 0, False)} # (delay, last time sensor data was updated, sensor data)
-                self.subscribedto[UID]['flip_time'] = self.calculateFlipTime(UID)
-            elif UID in self.UIDs and  UID in self.subscribedto:
-                self.subscribedto[UID]['delay'] = delay
+                self.subscribedTo[UID] = {'delay': delay, 'time': last_time}
+                self.subscribedTo[UID]['data'] = self.__getRandomData(UID, True)
+                self.subscribedTo[UID]['is_low'] = True
+                self.subscribedTo[UID]['flip_time'] = self.__calculateFlipTime(UID)
+            elif UID in self.UIDs and  UID in self.subscribedTo:
+                self.subscribedTo[UID]['delay'] = delay
         return 0
 
     def getData(self, UID):
-        delay, last_time = self.subscribedto[UID]['delay'], self.subscribedto[UID]['time']
-        flip_time = self.subscribedto[UID]['flip_time']
+        """ Extracts all data for specific UID. Checks whether to update data,
+        flip data, or return previous data, and returns correct appropriate
+        data.
+        """
+
+        delay = self.subscribedTo[UID]['delay']
+        last_time = self.subscribedTo[UID]['time']
+        flip_time = self.subscribedTo[UID]['flip_time']
         curr_time = time.time()*1000
-        if curr_time - delay >= flip_time:
-            self.subscribedto[UID]['time'] = curr_time #updates last time sensor data was updated to current system time
-            self.subscribedto[UID]['data'] =  self.getRandomData(UID, 0, True)#rewrites sensor data with random num
-            self.subscribedto[UID]['flip_time'] = self.calculateFlipTime(UID)
-        elif curr_time - delay < flip_time and curr_time - delay > last_time:
-            self.subscribedto[UID]['time'] = curr_time
-            self.subscribedto[UID]['data'] = self.getRandomData(UID, self.subscribedto[UID]['data'], False)
-        return self.subscribedto[UID]['data'] #returns sensor data
 
+        should_flip = curr_time - delay >= flip_time
+        should_update = curr_time - delay >= last_time
 
+        if should_flip and should_update:
+            self.subscribedTo[UID]['time'] = curr_time
+            self.subscribedTo[UID]['data'] =  self.__getRandomData(UID, self.subscribedTo[UID]['is_low'])
+            self.subscribedTo[UID]['flip_time'] = self.__calculateFlipTime(UID)
+            self.subscribedTo[UID]['is_low'] = not self.subscribedTo[UID]['is_low']
+        elif should_update:
+            self.subscribedTo[UID]['time'] = curr_time
+            self.subscribedTo[UID]['data'] = self.__getRandomData(UID, not self.subscribedTo[UID]['is_low'])
+        return self.subscribedTo[UID]['data'] #returns sensor data
+
+    def __calculateFlipTime(self, UID):
+        """ Determines time of flip. """
+
+        rand, last_time = random.random(), self.subscribedTo[UID]['time']
+        noise = random.random()
+        for i in range(len(cutoffs)):
+            if rand < cutoffs[i]:
+                return (time.time() + noise + i)*1000
+
+    def __getRandomData(self, UID, is_low):
+        """ Finds device_type of UID and returns corresponding flipped data."""
+
+        device_type = UID[:6]
+        low, high, noise = sensor_values[device_type]
+        if is_low:
+            return high + (random.random() - .5) * noise
+        else:
+            return low + (random.random() - .5) * noise
+
+############
+## MOTORS ##
+############
 
     def readValue(self, UID, param):
         #param values: 0 - delay, 1 - last_time, 2 - data
         try:
-            return self.subscribedto[UID][param]
+            return self.subscribedTo[UID][param]
         except:
             return 1
 
     def writeValue(self, UID, param, value):
-        if param >= len(self.subscribedto[UID]):
+        if param >= len(self.subscribedTo[UID]):
             return 1
-        self.subscribedto[UID][param] = value
+        self.subscribedTo[UID][param] = value
         return 0
 
 
-
-    def calculateFlipTime(self, UID):
-        rand, last_time = random.random(), self.subscribedto[UID]['time']
-        noise = random.random()
-        for i in range(len(cutoffs)):
-            if rand < cutoffs[i]:
-                return time.time() + noise + i
-
-
-
-    def getRandomData(self, UID, current_data, flip):
-        device_type = UID[:6]
-        low, high, noise = sensor_values[device_type]
-        average = (low + high) / 2
-        if current_data < average:
-            if flip:
-                return high + (random.random() - .5) * noise
-            else:
-                return low + (random.random() - .5) * noise
-        else:
-            if flip:
-                return low + (random.random() - .5) * noise
-            else:
-                return high + (random.random() - .5) * noise
-
-
+#############
+## TESTING ##
+#############
 
 hi = Hibike()
 hi.subscribeToDevices([('0x000100FFFFFFFFFFFFFFFF', 1)])
-print(hi.subscribedto)

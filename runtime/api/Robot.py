6f8f68fd160db0fd86311694e394bc0f42f3b19d
Matthew Zhao <matthew.x.zhao@berkeley.edu>
2015-12-05 14:25:33 -0800

    Readded docstrings to api/Robot.py

--- runtime/api/Robot.py
+++ runtime/api/Robot.py
@@ -11,12 +11,20 @@ mc = memcache.Client(['127.0.0.1:%d' % memcache_port]) # connect to memcache
 motor = {}
 
 def get_motor(name):
+    """Returns the current power value for a motor.
+    :param name: A string that identifies the motor
+    :returns: A value between -100 and 100, which is the power level of that motor.
+    """
     name_to_value = mc.get('motor_values')
     if name in name_to_value:
         return name_to_value[name]
     return 'Error, motor with that name not found'
 
 def set_motor(name, value):
+    """Sets a motor to the specified power value.
+    :param name: A string that identifies the motor
+    :param value: A decimal value between -100 and 100, the power level you want to set
+    """
     name_to_value = mc.get('motor_values')
     if name in name_to_value:
         name_to_value[name] = value
@@ -26,13 +34,19 @@ def set_motor(name, value):
 
 # TODO: implement
 def get_sensor(name):
+    """Returns the value, or reading corresponding to the specified sensor.
+    """
     all_data = mc.get('sensor_values')
     if name in all_data:
         return all_data[name]
     return 'Error, sensor with that name not found'
 
 def get_all_motors():
+    """Returns the current power values for every connected motor.
+    """
     return mc.get('motor_values')
 
 def set_timer(timestamp):
+    """ Setting timer
+    """
     mc.set('timestamp', timestamp)

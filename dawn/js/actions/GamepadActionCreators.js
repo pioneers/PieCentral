4ecf0867b3018106ea94d92b27e0660a528e54a2
Allan Zhou <allan.zhou@berkeley.edu>
2016-01-05 22:10:57 -1000

    Fix timestamp bug in gamepads

--- dawn/js/actions/GamepadActionCreators.js
+++ dawn/js/actions/GamepadActionCreators.js
@@ -7,7 +7,7 @@ var _timestamps = [0, 0, 0, 0];
 
 var _needToUpdate = function(newGamepads) {
   _.forEach(newGamepads, function(gamepad, index) {
-    if(gamepad && gamepad.timestamp > _timestampes[index]) {
+    if(gamepad && gamepad.timestamp > _timestamps[index]) {
       _timestamps[index] = gamepad.timestamp;
       return true;
     }

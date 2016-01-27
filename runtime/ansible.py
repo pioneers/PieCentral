f55152e16a8cd1d07c1dafe713693f2541750664
Allan Zhou <allan.zhou@berkeley.edu>
2016-01-27 00:18:56 -0800

    Finish ansible rewrite

--- runtime/ansible.py
+++ runtime/ansible.py
@@ -1,35 +1,6 @@
-from flask import Flask, copy_current_request_context, request
-from flask.ext.socketio import SocketIO
 from multiprocessing import Process, Queue
 from Queue import Empty
-
-def socket_server(recv_queue, send_queue):
-    app = Flask(__name__)
-    socketio = SocketIO(app)
-
-    @socketio.on('message')
-    def receive_message(msg):
-        print msg
-        recv_queue.put_nowait(msg)
-
-    @socketio.on('connect')
-    def on_connect():
-        socketio.emit('message', 'test')
-        print 'Connected to Dawn.'
-
-    @socketio.on_error()
-    def on_error(e):
-        print e
-
-    def send_process(send_queue):
-        while True:
-            msg = send_queue.get()
-            socketio.emit('message', msg)
-
-    send_p = Process(target=send_process, args=(send_queue,))
-    send_p.start()
-
-    socketio.run(app)
+from ansible_server import ansible_server
 
 # DON'T USE THIS UNLESS YOU KNOW WHAT YOU'RE DOING
 # Low level message sending. For high level messaging, use send_msg.
@@ -44,18 +15,15 @@ def send_message(msg_type, content):
     })
 
 # Receives a message, or None if there is no current message.
-# If block is True, blocks.
-def recv(block=False):
-    if block:
-        return recv_queue.get()
-    else:
-        try:
-            return recv_queue.get_nowait()
-        except Empty:
-            return None
+def recv():
+    try:
+        return recv_queue.get_nowait()
+    except Empty:
+        return None
 
-recv_queue = Queue()
+# Start up the Flask-SocketIO server
 send_queue = Queue()
+recv_queue = Queue()
+ansible_p = Process(target=ansible_server, args=(send_queue, recv_queue))
+ansible_p.start()
 
-socket_p = Process(target=socket_server, args=(recv_queue, send_queue))
-socket_p.start()
---
+++ runtime/ansible_server.py
@@ -0,0 +1,64 @@
+async_mode = None
+
+if async_mode is None:
+    try:
+        import eventlet
+        async_mode = 'eventlet'
+    except ImportError:
+        pass
+
+    if async_mode is None:
+        try:
+            from gevent import monkey
+            async_mode = 'gevent'
+        except ImportError:
+            pass
+
+    if async_mode is None:
+        async_mode = 'threading'
+
+    print 'async_mode is ' + async_mode
+
+if async_mode == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+
+from threading import Thread
+from Queue import Empty
+import json
+import time
+from flask import Flask, copy_current_request_context, request
+from flask.ext.socketio import SocketIO
+
+# A Flask server with the Socket.IO extension to communicate with Dawn.
+def ansible_server(send_queue, recv_queue):
+    app = Flask(__name__)
+    socketio = SocketIO(app)
+
+    @socketio.on('message')
+    def receive_message(msg):
+        recv_queue.put_nowait(json.loads(msg))
+
+    @socketio.on('connect')
+    def on_connect():
+        print 'Connected to Dawn.'
+
+    @socketio.on_error()
+    def on_error(e):
+        print e
+
+    def send_process(send_queue):
+        while True:
+            try:
+                msg = send_queue.get_nowait()
+                socketio.emit('message', msg)
+                time.sleep(.02)
+            except Empty:
+                time.sleep(.02)
+
+    send_p = Thread(target=send_process, args=(send_queue,))
+    send_p.daemon = True
+    send_p.start()
+
+    socketio.run(app)
+

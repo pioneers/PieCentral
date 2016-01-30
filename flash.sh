0e86382827214f5163c231b6c84164c141b10d92
AravindK95 <aravind.kumaraguru@gmail.com>
2016-01-30 15:42:42 -0800

    Created flash script flash.sh - Usage: $./flash.sh <DeviceName> - Remember to run 'make clean' between flashing different types of   devices.

--- flash.sh
+++ flash.sh
@@ -1,14 +1,7 @@
 #/bin/bash
 
-echo Putting smartsensor into reset mode
-echo
-screen -D -m -S 'reset' /dev/ttyACM1 1200 &
-sleep 0.5
-screen -X -S reset quit
-sleep 1
+python reset.py /dev/ttyACM*
+sleep 3
+echo "device is:  $1"
 
-
-echo
-echo Programming smartsensor
-# /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C/Users/bduckering/Library/Arduino15/packages/SparkFun/hardware/avr/1.0.3/avrdude.conf -v -patmega32u4 -cavr109 -P/dev/cu.usbmodem1421 -b57600 -D -Uflash:w:"$1":i
-make upload
+make upload DEVICE=$1

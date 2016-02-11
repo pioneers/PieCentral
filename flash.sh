970003ae6d0bb039cbfd8a8157298202555113a8
AravindK95 <aravind.kumaraguru@gmail.com>
2016-02-10 19:57:09 -0800

    Modified flash.sh for E-Prod use.

--- flash.sh
+++ flash.sh
@@ -2,6 +2,9 @@
 
 python reset.py /dev/ttyACM*
 sleep 3
-echo "device is:  $1"
+# echo "device is:  $1"
+# /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C/Users/bduckering/Library/Arduino15/packages/SparkFun/hardware/avr/1.0.3/avrdude.conf -v -patmega32u4 -cavr109 -P/dev/cu.usbmodem1421 -b57600 -D -Uflash:w:"$1":i
+avrdude -v -patmega32u4 -cavr109 -P/dev/ttyACM* -b57600 -D -Uflash:w:"$1":i
 
-make upload DEVICE=$1
+# make upload DEVICE=$1
+

07888da19f9978a29d1ac38aa67497a93604942b
Casey Duckering <cduckering@gmail.com>
2016-02-13 21:04:38 -0800

    Improved the Battery Buzzer: measurement hysteresis, calibration, and precise measurement frequency with 52 sample averaging

--- devices/BatteryBuzzer/battery_buzzer.cpp
+++ devices/BatteryBuzzer/battery_buzzer.cpp
@@ -1,96 +1,263 @@
+#include "EEPROM.h"
 #include "battery_buzzer.h"
 
 uint8_t safe, connected;
-float cell_voltages[NUM_CELLS], v_total;
+
+// Variables used in the ADC read timer interrupt
+const uint8_t cellPins[NUM_CELLS] = BATT_INPUT;
+const uint8_t channelNums[NUM_CELLS] = BATT_CHANNELS;  // The ADC channels for A0, A1, A2 on atmega32u4
+uint16_t analogAccumArrs[2][NUM_CELLS] = {{0, 0, 0}, {0, 0, 0}};
+float cellVoltageArrs[2][NUM_CELLS] = {{0, 0, 0}, {0, 0, 0}};
+float cellTotalArr[2] = {0, 0};
+uint8_t disconnectedArr[2] = {0, 0};
+volatile uint8_t latestAnalogAccum = 0;  // 0 or 1 index into analogAccumArrs, 0xFF means no data yet
+volatile uint8_t lastInputIndex = 0;
+volatile uint8_t inputIndex = 0;
+volatile uint8_t inputSample = 0;
+volatile uint32_t lastUpdateTime = 0;
+volatile float cellScale[NUM_CELLS] = DEFAULT_CALIBRATION;
+int measureCounter = 0;
+uint32_t lastMeasureTime = 0;
 
 // unbalanced for each cell is relative to the cell after it mod 3
-float cell_readings[NUM_CELLS];
-int cell_pins[NUM_CELLS] = {BATT_0, BATT_1, BATT_2};
 cell_state cell_states[NUM_CELLS] = {CELL_SAFE, CELL_SAFE, CELL_SAFE};
+
 // normal arduino setup function, you must call hibike_setup() here
 void setup() {
-  pinMode(BATT_0, INPUT);
-  pinMode(BATT_1, INPUT);
-  pinMode(BATT_2, INPUT);
+  for (int i=0; i<NUM_CELLS; i++) {
+    pinMode(cellPins[i], INPUT);
+  }
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BUZZER_LED_PIN, OUTPUT);
   pinMode(READ_ENABLE_PIN, OUTPUT);
-  read_voltage();
+  digitalWrite(READ_ENABLE_PIN, HIGH);
+  digitalWrite(BUZZER_LED_PIN, HIGH);
+
+  calibrationSetup(false);
+  calibrationSetup(true);  // Also plays startup tone
+  readTimerStart();
+
+  delay(100);  // Wait for the first reading to finish (50ms minimum)
+
   hibike_setup();
 }
 
 // normal arduino loop function, you must call hibike_loop() here
 // hibike_loop will look for any packets in the serial buffer and handle them
 void loop() {
-  read_voltage();
-  if (safe) {
-    noTone(BUZZER_PIN);
-  } else {
-    tone(BUZZER_PIN, BUZZER_FREQ);
+  cli();
+  uint32_t lastTime = lastMeasureTime;
+  sei();
+  if (millis() - lastTime > MEASURE_DELAY_UNSAFE_MS) {
+    // Should never happen
+    // Indicates that the timer interrupt stopped working
+    tone(BUZZER_PIN, BUZZER_BROKEN_FREQ);
   }
   hibike_loop();
 }
 
-// returns new state of a cell assuming currently safe
-cell_state new_state(size_t cell) {
-    if (cell_readings[cell] < CONNECTED_ENTER_THRESHOLD) {
-      return CELL_DISCONNECTED;
-    } else if (cell_voltages[cell] < LOW_VOLTAGE_ENTER_THRESHOLD) {
-      return CELL_LOW_VOLTAGE;
-    } else if (abs_diff(cell_voltages[cell], cell_voltages[(cell + 1) % NUM_CELLS]) > UNBALANCED_ENTER_THRESHOLD) {
-      return CELL_UNBALANCED;
-    } else {
-      return CELL_SAFE;
+void calibrationSetup(bool beep) {
+  uint8_t arrIndex = latestAnalogAccum;
+  memset(analogAccumArrs[arrIndex], 0, NUM_CELLS*sizeof(uint16_t));
+  for (int i=0; i<NUM_SAMPLES; i++) {
+    for (int j=0; j<NUM_CELLS; j++) {
+      analogAccumArrs[arrIndex][j] += analogRead(cellPins[j]);
     }
+  }
+  bool shouldCalibrate = true;
+  for (int j=0; j<NUM_CELLS; j++) {
+    cellVoltageArrs[arrIndex][j] = analogAccumArrs[arrIndex][j]*cellScale[j];
+    if (cellVoltageArrs[arrIndex][j] < MIN_REF_VOLT || cellVoltageArrs[arrIndex][j] > MAX_REF_VOLT) {
+      shouldCalibrate = false;
+    }
+  }
+  cellTotalArr[arrIndex] = cellVoltageArrs[arrIndex][2];
+  cellVoltageArrs[arrIndex][2] -= cellVoltageArrs[arrIndex][1];
+  cellVoltageArrs[arrIndex][1] -= cellVoltageArrs[arrIndex][0];
+  if (shouldCalibrate) {
+    // Store calibration to EEPROM
+    EEPROM.write(0, 'N');
+    for (int j=0; j<NUM_CELLS; j++) {
+      cellScale[j] = ((float)REF_VOLT)/analogAccumArrs[arrIndex][j];
+      uint8_t *scaleBytes = (uint8_t*)(void*)&cellScale[j];
+      for (int k=0; k<4; k++) {
+        EEPROM.write(4+j*4+k, scaleBytes[k]);
+      }
+    }
+    EEPROM.write(0, 'C');
+    EEPROM.write(1, 'A');
+    EEPROM.write(2, 'L');
+    EEPROM.write(3, ':');
+    if (beep) {
+      tone(BUZZER_PIN, NOTE_C3, 0);
+      delay(250);
+      tone(BUZZER_PIN, NOTE_D3, 0);
+      delay(250);
+      noTone(BUZZER_PIN);
+    }
+  } else if(EEPROM.read(0)=='C' && EEPROM.read(1)=='A' && EEPROM.read(2)=='L' && EEPROM.read(3)==':') {
+    // Read calibration from EEPROM
+    for (int j=0; j<NUM_CELLS; j++) {
+      uint8_t *scaleBytes = (uint8_t*)(void*)&cellScale[j];
+      for (int k=0; k<4; k++) {
+        scaleBytes[k] = EEPROM.read(4+j*4+k);
+      }
+    }
+    if (beep) {
+      tone(BUZZER_PIN, NOTE_C4, 0);
+      delay(250);
+      tone(BUZZER_PIN, NOTE_D4, 0);
+      delay(250);
+      noTone(BUZZER_PIN);
+    }
+  } else {
+    // Otherwise don't calibrate
+    if (beep) {
+      tone(BUZZER_PIN, NOTE_C3, 0);
+      delay(250);
+      tone(BUZZER_PIN, NOTE_B2, 0);
+      delay(250);
+      noTone(BUZZER_PIN);
+    }
+  }
 }
 
-void read_voltage() {
-  digitalWrite(READ_ENABLE_PIN, HIGH);
+void readTimerStart() {
+  // Configure ADC
+  ADMUX = (1<<REFS0);  // Set reference voltage to AVcc (5V)
+  ADCSRA = (1<<ADEN) | (7<<ADPS0);  // Enable ADC, set divider to 128 (125kHz when 16MHz clock)
 
-  // BATT_0 = 1/2 * v_0
-  // BATT_1 = 1/4 * (v_0 + v_1)
-  // BATT_2 = 10/61 * (v_0 + v_1 + v_2)
-  size_t cell;
-  for (cell = 0; cell < NUM_CELLS; cell++) {
-    cell_readings[cell] = analogRead(cell_pins[cell]);
-  }
-  digitalWrite(READ_ENABLE_PIN, LOW);
+  // Setup Timer 1 (Timer 0 is for Arduino millis/micro/delay, Timer 3 is for tone)
+  // Configure to run at 3125 Hz (every 320 us)
+  cli();
+  TCCR1A = 0;//(3<<COM3A0);// | (0<<WGM10);  // Reset timer on compare and enable compare A
+  TCCR1B = (1<<WGM12) | (5<<CS10);    // Set divider to 1024 (~16kHz for 16MHz clock)
+  OCR1A = 4;  // Set period to 5=(4+1) -> 3125Hz
+  TCNT1 = 0;  // Reset timer value
+  TIFR1 = (1<<OCF1A);  // Clear previous interrupt flag
+  TIMSK1 = (1<<OCIE1A);  // Enable interrupt on compare A
+  sei();
+}
 
-  cell_voltages[0] = (float) cell_readings[0] * 2.0 * VOLTS_PER_UNIT;
-  cell_voltages[1] = (float) cell_readings[1] * 4.0 * VOLTS_PER_UNIT - cell_voltages[0];
-  cell_voltages[2] = (float) cell_readings[2] * 6.1 * VOLTS_PER_UNIT - (cell_voltages[1] + cell_voltages[0]);
-  v_total = cell_voltages[0] + cell_voltages[1] + cell_voltages[2];
-
-  // update cell_states
-  for (cell = 0; cell < NUM_CELLS; cell++) {
-    switch (cell_states[cell]) {
-      case CELL_SAFE:
-        cell_states[cell] = new_state(cell);
-        break;
-      case CELL_DISCONNECTED:
-        if (cell_readings[cell] > DISCONNECTED_EXIT_THRESHOLD) {
-          cell_states[cell] = new_state(cell);
-        }
-        break;
-      case CELL_LOW_VOLTAGE:
-        if (cell_voltages[cell] > LOW_VOLTAGE_EXIT_THRESHOLD) {
-          cell_states[cell] = new_state(cell);
-        }
-        break;
-      case CELL_UNBALANCED:
-        if (abs_diff(cell_voltages[cell], cell_voltages[(cell + 1) % NUM_CELLS]) > UNBALANCED_EXIT_THRESHOLD) {
-          cell_states[cell] = new_state(cell);
-        }
-        break;
-    } 
-  }
-  safe = 1;
-  connected = 1;
-  for (cell = 0; cell < NUM_CELLS; cell++) {
-    if (cell_states[cell] == CELL_DISCONNECTED) {
-      connected = 0;
+// Run at 3125 Hz (every 320 us)
+ISR(TIMER1_COMPA_vect) {
+  uint16_t nextVal = ADC;  // Assume that the previous conversion finished
+  // Start next analog read
+  ADMUX = (ADMUX & 0xF8) | (channelNums[inputIndex] & 0x07);  // Select ADC channel
+  ADCSRA |= (1<<ADSC);  // Start conversion
+  // Accumulate inputs so an average analog input can be generated
+  if(lastInputIndex == inputIndex) {
+    // This is the first time
+    // there is no data yet
+    inputIndex++;
+    // Zero the new accumulation array
+    memset(analogAccumArrs[!latestAnalogAccum], 0, NUM_CELLS*sizeof(uint16_t));
+    disconnectedArr[!latestAnalogAccum] = 0;
+  } else {
+    analogAccumArrs[!latestAnalogAccum][lastInputIndex] += nextVal;  // Write data to the other output array
+    disconnectedArr[!latestAnalogAccum] += nextVal < DISCONNECTED_THRESHOLD;
+    if (disconnectedArr[!latestAnalogAccum] > DISCONNECTED_COUNT) {
+      disconnectedArr[!latestAnalogAccum] = DISCONNECTED_COUNT;
     }
-    if (cell_states[cell] != CELL_SAFE) {
-      safe = 0;
+    if (inputIndex == 0 && inputSample == 0) {
+      uint8_t arrIndex = !latestAnalogAccum;
+      cellVoltageArrs[arrIndex][0] = analogAccumArrs[arrIndex][0]*cellScale[0];
+      cellVoltageArrs[arrIndex][1] = analogAccumArrs[arrIndex][1]*cellScale[1];
+      cellTotalArr[arrIndex] = analogAccumArrs[arrIndex][2]*cellScale[2];
+      cellVoltageArrs[arrIndex][2] = cellTotalArr[arrIndex] - cellVoltageArrs[arrIndex][1];
+      cellVoltageArrs[arrIndex][1] -= cellVoltageArrs[arrIndex][0];
+      
+      // Just finished taking samples, output the total now
+      latestAnalogAccum = !latestAnalogAccum;
+      // Zero the new accumulation array
+      memset(analogAccumArrs[!latestAnalogAccum], 0, NUM_CELLS*sizeof(uint16_t));
+      disconnectedArr[!latestAnalogAccum] = 0;
+      measureCounter++;
+      if (measureCounter >= BLINK_AFTER_MEASURE) measureCounter = 0;
+      if (measureCounter == 0) {
+        digitalWrite(BUZZER_LED_PIN, HIGH);
+      }
+      if (measureCounter == 1) {
+        digitalWrite(BUZZER_LED_PIN, LOW);
+      }
+
+      // Update states and turn on/off buzzer
+      updateCellStates();
+      updateSafety();
+      lastMeasureTime = millis();
     }
+    lastInputIndex = inputIndex;
+    if (inputIndex < NUM_CELLS-1) {
+      inputIndex++;
+    }
+    else {
+      inputIndex = 0;
+      if (inputSample < NUM_SAMPLES-1) {
+        inputSample++;
+      } else {
+        inputSample = 0;
+      }
+    }
+  }
+}
+
+void updateSafety() {
+  if (disconnectedArr[latestAnalogAccum] >= DISCONNECTED_COUNT) {
+    tone(BUZZER_PIN, BUZZER_DISCONNECT_FREQ);
+    connected = false;
+    safe = false;
+  } else {
+    connected = true;
+    safe = true;
+    for (int i=0; i<NUM_CELLS; i++) {
+      if (cell_states[i] != CELL_SAFE) {
+        safe = false;
+      }
+    }
+    if (safe) {
+      noTone(BUZZER_PIN);
+    } else {
+      tone(BUZZER_PIN, BUZZER_FREQ);
+    }
+  }
+}
+
+void updateCellStates() {
+  cell_states[0] = new_state(cellVoltageArrs[latestAnalogAccum][0], cellVoltageArrs[latestAnalogAccum][1],
+                             disconnectedArr[latestAnalogAccum] >= DISCONNECTED_COUNT, cell_states[0]);
+  cell_states[1] = new_state(cellVoltageArrs[latestAnalogAccum][1], cellVoltageArrs[latestAnalogAccum][2],
+                             disconnectedArr[latestAnalogAccum] >= DISCONNECTED_COUNT, cell_states[1]);
+  cell_states[2] = new_state(cellVoltageArrs[latestAnalogAccum][2], cellVoltageArrs[latestAnalogAccum][0],
+                             disconnectedArr[latestAnalogAccum] >= DISCONNECTED_COUNT, cell_states[2]);
+}
+
+// returns new state of a cell
+cell_state new_state(float voltage, float nextCellVoltage, bool disconnected, cell_state lastState) {
+  // Entering unsafe state
+  if (disconnected) {
+    return CELL_DISCONNECTED;
+  } else if (voltage < LOW_VOLTAGE_ENTER_THRESHOLD) {
+    return CELL_LOW_VOLTAGE;
+  } else if (abs_diff(voltage, nextCellVoltage) > UNBALANCED_ENTER_THRESHOLD) {
+    return CELL_UNBALANCED;
+  }
+  // Exiting unsafe state
+  switch (lastState) {
+    case CELL_LOW_VOLTAGE:
+      if (voltage > LOW_VOLTAGE_EXIT_THRESHOLD) {
+        if (abs_diff(voltage, nextCellVoltage) > UNBALANCED_ENTER_THRESHOLD) return CELL_UNBALANCED;
+        else return CELL_SAFE;
+      }
+      return CELL_LOW_VOLTAGE;
+    case CELL_UNBALANCED:
+      if (lastState == CELL_UNBALANCED && abs_diff(voltage, nextCellVoltage) < UNBALANCED_EXIT_THRESHOLD) {
+        return CELL_SAFE;
+      }
+      return CELL_UNBALANCED;
+    case CELL_SAFE:
+    case CELL_DISCONNECTED:
+    default:
+      return CELL_SAFE;
   }
 }
 
@@ -114,7 +281,6 @@ uint32_t device_status(uint8_t param) {
   return ~((uint32_t) 0);
 }
 
-
 // you must implement this function. It is called with a buffer and a maximum buffer size.
 // The buffer should be filled with appropriate data for a DataUpdate packer, and the number of bytes
 // added to the buffer should be returned. 
@@ -122,14 +288,16 @@ uint32_t device_status(uint8_t param) {
 // You can use the helper function append_buf.
 // append_buf copies the specified amount data into the dst buffer and increments the offset
 uint8_t data_update(uint8_t* data_update_buf, size_t buf_len) {
-  if (buf_len < (sizeof(safe) + sizeof(connected) + sizeof(cell_voltages) + sizeof(v_total))) {
+  if (buf_len < (sizeof(safe) + sizeof(connected) + sizeof(cellVoltageArrs[0][0]*NUM_CELLS) + sizeof(cellTotalArr[0]))) {
     return 0;
   }
   uint8_t offset = 0;
+  cli();
   append_buf(data_update_buf, &offset, (uint8_t *)&safe, sizeof(safe));
   append_buf(data_update_buf, &offset, (uint8_t *)&connected, sizeof(connected));
-  append_buf(data_update_buf, &offset, (uint8_t *)&cell_voltages, sizeof(cell_voltages));
-  append_buf(data_update_buf, &offset, (uint8_t *)&v_total, sizeof(v_total));
+  append_buf(data_update_buf, &offset, (uint8_t *)&cellVoltageArrs[latestAnalogAccum][0], sizeof(cellVoltageArrs[0][0])*NUM_CELLS);
+  append_buf(data_update_buf, &offset, (uint8_t *)&cellTotalArr[latestAnalogAccum], sizeof(cellTotalArr[0]));
+  sei();
   return offset;
 }
 

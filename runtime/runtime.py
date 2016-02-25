d24ea18dbd1490141c7e11eeab22a126161fcfef
Matthew Zhao <matthew.x.zhao@berkeley.edu>
2016-02-24 21:47:11 -0800

    Fixed yi's thing

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -270,7 +270,7 @@ def uid_to_device_id(uid, num_devices):
     return [ str(uid)+str(device_index) for device_index in range(1,1+num_devices)]
 
 def device_id_to_uid(device_id):
-    return device_id[:-1]
+    return int(device_id[:-1])
 
 
 init_battery()

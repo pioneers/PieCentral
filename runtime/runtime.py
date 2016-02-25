bef0cf2b3da991105d55894a3bdd8db87646419f
Matthew Zhao <matthew.x.zhao@berkeley.edu>
2016-02-25 12:03:39 -0800

    Fixed merge conflicts with sudo restart runtime

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -243,7 +243,7 @@ def msg_handling(msg):
             console_proc.terminate()
             stop_motor()
 
-        os.system('sh ~/updates/update.sh')
+        os.system('sudo restart runtime')
 
 peripheral_data_last_sent = 0
 def send_peripheral_data(data):

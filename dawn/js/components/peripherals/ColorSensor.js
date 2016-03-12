dc24758e842cc51aee9688fc17e93444fa0b6f5e
Allan Zhou <allan.zhou@berkeley.edu>
2016-03-12 14:55:47 -0800

    Graphical colorsensor display

--- dawn/js/components/peripherals/ColorSensor.js
+++ dawn/js/components/peripherals/ColorSensor.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NameEdit from './NameEdit';
 import numeral from 'numeral';
+import _ from 'lodash';
 
 var ColorSensor = React.createClass({
   propTypes: {
@@ -9,12 +10,30 @@ var ColorSensor = React.createClass({
     id: React.PropTypes.string,
     value: React.PropTypes.array
   },
+  /* Convert the separate R, G, B integer values to hex representation,
+   * with the format '#RRGGBB'
+   */
+  getHexColorValue() {
+    let hexRepresentation = _.map(_.slice(this.props.value, 0, 3), (val)=>{
+      let hexVal = val.toString(16);
+      return hexVal.length === 1 ? '0' + hexVal : hexVal;
+    }).join('');
+    return '#' + hexRepresentation;
+  },
   render() {
     return (
     <div style={{overflow: 'auto'}}>
       <div style={{overflow: 'auto', width: '100%'}}>
-        <h4 style={{float: 'left'}}><NameEdit name={this.props.name} id={this.props.id} /><small> {this.props.peripheralType} </small></h4>
-        <h4 style={{float: 'right'}}>Hue: {this.props.value[4]}</h4>
+        <h4 style={{float: 'left'}}>
+          <NameEdit name={this.props.name} id={this.props.id} />
+          <small> {this.props.peripheralType} </small>
+        </h4>
+        <div style={{
+          float: 'right',
+          width: '35px',
+          height: '35px',
+          backgroundColor: this.getHexColorValue()}}>
+        </div>
       </div>
     </div>
     );

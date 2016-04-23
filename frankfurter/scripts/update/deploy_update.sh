b337ff473db44dccd0d5aa2a8eb9ba797041f730
Vincent Donato <vincentdonato@pioneers.berkeley.edu>
2016-04-23 14:20:19 -0700

    Update runtime deploy script.

--- frankfurter/scripts/update/deploy_update.sh
+++ frankfurter/scripts/update/deploy_update.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 FRANKFURTER_DIR=$(git rev-parse --show-toplevel)/frankfurter
+IP=192.168.13.100
+
 cd $FRANKFURTER_DIR
 
 if ! ls build/frankfurter-update-* 1> /dev/null 2>&1; then
     make create_update
 fi
 
-scp build/* ubuntu@192.168.13.100:~/updates/
-ssh ubuntu@192.168.13.100 'sudo restart runtime'
+
+ssh ubuntu@$IP 'rm -rf ~/updates/*.tar.gz* && rm -rf ~/updates/temp'
+scp build/* ubuntu@$IP:~/updates/
+ssh ubuntu@$IP 'sudo restart runtime'

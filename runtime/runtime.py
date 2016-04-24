cecc425a5fc8f86b3bd8dda9d0638cedf13b0ff0
YiDing Jiang <yidinggjiangg@gmail.com>
2016-04-23 23:28:56 -0700

    stop console print in competition mode

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -508,12 +508,13 @@ def msg_handling(msg):
         student_proc = subprocess.Popen(['python', '-u', 'student_code/student_code.py'],
                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         # turns student process stdout into a stream for sending to frontend
-        lines_iter = iter(student_proc.stdout.readline, b'')
-        # start process for watching for student code output
-        robot_status= 1
-        console_proc = Thread(target=log_output,
-                              args=(lines_iter,))
-        console_proc.start()
+        if mc.get('competition')[competition]:
+            lines_iter = iter(student_proc.stdout.readline, b'')
+            # start process for watching for student code output
+            robot_status= 1
+            console_proc = Thread(target=log_output,
+                                  args=(lines_iter,))
+            console_proc.start()
     elif msg_type == 'stop' and robot_status:
         student_proc.terminate()
         # console_proc.terminate()
@@ -574,6 +575,8 @@ def send_motor_data(data):
 read_naming_map()
 enumerate_hibike()
 enumerate_motors()
+mc.set('competition', {'competition': False})
+
 while True:
     if battery_UID: #TODO Only tests battery safety if battery buzzer is connected
         battery_safe = test_battery()

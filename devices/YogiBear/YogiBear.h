4a3036ab2638bceb9ea9ca215419d03e9497da90
Stanley Liu <stanley.liu@berkeley.edu>
2016-07-30 16:44:03 -0700

    initial commit for Yogi Bear

---
+++ devices/YogiBear/YogiBear.h
@@ -0,0 +1,24 @@
+#ifndef EX_DEVICE_H
+#define EX_DEVICE_H
+
+#include "hibike_device.h"
+
+//////////////// DEVICE UID ///////////////////
+hibike_uid_t UID = {
+  YOGI_BEAR,                      // Device Type
+  0,                      // Year
+  UID_RANDOM,     // ID
+};
+///////////////////////////////////////////////
+
+
+
+#define NUM_PARAMS 4
+
+
+// function prototypes
+void setup();
+void loop();
+
+
+#endif /* YOGI_BEAR_FOLLOWER_H */

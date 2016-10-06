3fad3e5b2791c14d5aeae3fec1311ab70b8fba40
Jonathan Lee <jonathan-j-lee@users.noreply.github.com>
2016-10-05 20:30:26 -0700

    [DevOps] Fixes for update scripts (#42)
    * Removed tarball signing
    
    * Replaced disabling SSH host check hack
    
    * Specified which remote/branch to pull from when packaging update

--- DevOps/frankfurter/scripts/update/create_update.sh
+++ DevOps/frankfurter/scripts/update/create_update.sh
@@ -6,25 +6,36 @@
 # the file so that the signature can be verified before the robot uses the tarball to update #
 ##############################################################################################
 
-FRANKFURTER_DIR=$(git rev-parse --show-toplevel)/frankfurter
+FRANKFURTER_DIR=$(git rev-parse --show-toplevel)/DevOps/frankfurter
 BUILD_DIR=$FRANKFURTER_DIR/build
-TEMP_DIR=$FRANKFURTER_DIR/build/temp
+TMP_DIR=$BUILD_DIR/tmp
 
-rm -rf $BUILD_DIR
-mkdir -p $BUILD_DIR
-mkdir $TEMP_DIR
-# currently unused, but may be useful in the case that we have to build packages from source
+if [ -d "$BUILD_DIR" ]; then
+    echo "Cleaning old builds ... "
+    rm -rf $BUILD_DIR/*
+else
+    echo "Making build directory ... "
+    mkdir -p $BUILD_DIR
+fi
+mkdir -p $TMP_DIR
+echo "Done."
 
-git clone https://github.com/pioneers/daemon --branch develop --single-branch $TEMP_DIR/daemon
-git clone https://github.com/pioneers/hibike $TEMP_DIR/hibike
-git clone https://github.com/pioneers/frankfurter $TEMP_DIR/frankfurter
-cp $FRANKFURTER_DIR/scripts/update/install_update.sh $TEMP_DIR
+git pull origin master
+cp -R $(git rev-parse --show-toplevel)/hibike $TMP_DIR/hibike
+# Copy as necessary
+# cp -R $(git rev-parse --show-toplevel)/runtime $TMP_DIR/runtime
+cp $FRANKFURTER_DIR/scripts/update/install_update.sh $TMP_DIR
 
 CURRENT_TIME=$(date +%s%N)
-UPDATE_FILE_NAME=frankfurter-update-"$CURRENT_TIME".tar.gz
+UPDATE_FILENAME=frankfurter-update-"$CURRENT_TIME".tar.gz
 
-cd $BUILD_DIR
-tar -zcf $UPDATE_FILE_NAME --directory=$TEMP_DIR .
-gpg --armor --detach-sign $UPDATE_FILE_NAME # create a signature for the tarball.
+cd $TMP_DIR
+tar -zcf $BUILD_DIR/$UPDATE_FILENAME *
+printf "\x1b[1;32m"
+echo "==> Tarball available at: "
+echo $BUILD_DIR/$UPDATE_FILENAME
+printf "\x1b[0m"
 
-rm -rf $TEMP_DIR
+cd ..
+rm -rf $TMP_DIR
+echo "Cleaned up."
--- DevOps/frankfurter/scripts/update/deploy_update.sh
+++ DevOps/frankfurter/scripts/update/deploy_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FRANKFURTER_DIR=$(git rev-parse --show-toplevel)/frankfurter
+FRANKFURTER_DIR=$(git rev-parse --show-toplevel)/DevOps/frankfurter
 IP=192.168.13.100
 
 cd $FRANKFURTER_DIR
@@ -9,10 +9,8 @@ if ! ls build/frankfurter-update-* 1> /dev/null 2>&1; then
     make create_update
 fi
 
-
-mv ~/.ssh/known_hosts ~/.ssh/known_hosts_backup
-ssh ubuntu@$IP 'rm -rf ~/updates/*.tar.gz* && rm -rf ~/updates/temp'
-scp build/* ubuntu@$IP:~/updates/
-ssh ubuntu@$IP 'sudo restart runtime'
-rm -f ~/.ssh/known_hosts
-mv ~/.ssh/known_hosts_backup ~/.ssh/known_hosts
+# Disable host fingerprinting
+OPTIONS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+ssh $OPTIONS ubuntu@$IP 'rm -rf ~/updates/*.tar.gz* && rm -rf ~/updates/temp'
+scp $OPTIONS build/* ubuntu@$IP:~/updates/
+ssh $OPTIONS ubuntu@$IP 'sudo restart runtime'

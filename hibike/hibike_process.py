f8bd1db5b79d35b75db6e1add03dbaa14010124e
karthik-shanmugam <karthik@karthiks.me>
2017-02-10 22:36:50 -0800

    [HIBIKE] handle errors when opening serial port in hibike_process.py (#123)
    When hibike_process.py failed to open a serial port, for example when one of the virtual devices in virtual_devices.txt turned out to be invalid, it would crash the whole process. This fix is a hack that instead only crashes the individual worker threads corresponding to the offending serial port. This fix should be replaced with an overall better management of worker threads in a later pull request.

--- hibike/hibike_process.py
+++ hibike/hibike_process.py
@@ -22,7 +22,17 @@ def hibike_process(badThingsQueue, stateQueue, pipeFromChild):
     except IOError:
         pass
 
-    serials = [serial.Serial(port, 115200) for port in ports]
+    serials = []
+
+    for port in ports:
+        try:
+            serials.append(serial.Serial(port, 115200))
+
+        # this is a hack to handle this error. 
+        # It pushes the error down to the worker threads for this device; they will try to do serial operations on a None object
+        # TODO karthik-shanmugam 97hongjun: implement cleaner management of worker threads that handles issues like this.
+        except serial.serialutil.SerialException as e:
+            serials.append(None)
 
     # each device has it's own write thread, with it's own instruction queue
     instruction_queues = [queue.Queue() for _ in ports]

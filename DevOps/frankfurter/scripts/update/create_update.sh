0b8e7a3cd6f3e795c76ce22ccb235e73afd9b612
Nikita Kitaev <nikitakit@gmail.com>
2017-02-15 20:55:57 -0800

    [DevOps] Update create_update.sh
    Build the protobuf python files, and don't automatically pull from
    GitHub (that prevents using this script inside travis)

--- DevOps/frankfurter/scripts/update/create_update.sh
+++ DevOps/frankfurter/scripts/update/create_update.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 ##############################################################################################
-# Pulls the newest versions of PieCentral from github and packages hibike and runtime nicely #
-# in a tarball along with the install_update.sh script.                                      #
+# Packages hibike and runtime nicely in a tarball along with the install_update.sh script.   #
 ##############################################################################################
+echo "Don't forget to pull the latest code before running this script!"
+echo "Current commit is: $(git rev-parse HEAD)"
+echo
 
 FRANKFURTER_DIR=$(git rev-parse --show-toplevel)/DevOps/frankfurter
+PROTO_DIR=$(git rev-parse --show-toplevel)/ansible-protos
 BUILD_DIR=$FRANKFURTER_DIR/build
 TMP_DIR=$BUILD_DIR/tmp
 
@@ -19,10 +22,10 @@ fi
 mkdir -p $TMP_DIR
 echo "Done."
 
-git pull origin master
 # Copy hibike and runtime
 cp -R $(git rev-parse --show-toplevel)/hibike $TMP_DIR/hibike
 cp -R $(git rev-parse --show-toplevel)/runtime $TMP_DIR/runtime
+protoc -I=$PROTO_DIR --python_out=$TMP_DIR/runtime/testy $PROTO_DIR/*.proto
 cp $FRANKFURTER_DIR/scripts/update/install_update.sh $TMP_DIR
 
 CURRENT_TIME=$(date +%s%N)

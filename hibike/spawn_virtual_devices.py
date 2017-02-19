3ec639c64b035619bd6a8dc1d514e4ab73c652d1
karthik-shanmugam <karthik@karthiks.me>
2017-02-18 17:41:12 -0800

    [HIBIKE] document usage of virtual devices (#125)
    - add a markdown file documenting the usage of virtual devices
    - modify `spawn_virtual_devices.py`, moving the user editable section to the top.
    - change the example list of virtual devices in `spawn_virtual_devices.py`
      - demonstrate all 3 supported device types
      - demonstrate support for spawning multiple devices of the same type
    - minor bugfix: invoke `python3` instead of `python3.5` , since virtual devices don't require python3.5

--- hibike/spawn_virtual_devices.py
+++ hibike/spawn_virtual_devices.py
@@ -7,12 +7,12 @@ import atexit
 import time
 
 popens = []
+devices_to_spawn = ["LimitSwitch", "LimitSwitch", "ServoControl", "Potentiometer"]
 
 def get_virtual_ports():
     socat = subprocess.Popen(shlex.split("socat -d -d pty,raw,echo=0 pty,raw,echo=0"), stderr=subprocess.PIPE)
     popens.append(socat)
     lines = [str(socat.stderr.readline()), str(socat.stderr.readline())]
-    #print(lines)
     p1 = re.search(r'PTY is (/dev/pts/\d+)', lines[0]).group(1)
     p2 = re.search(r'PTY is (/dev/pts/\d+)', lines[1]).group(1)
     return p1, p2
@@ -20,7 +20,7 @@ def get_virtual_ports():
 def spawn_device(device_type):
     p1, p2 = get_virtual_ports()
     fname = os.path.join(os.path.dirname(__file__), "virtual_device.py")
-    device = subprocess.Popen(shlex.split("python3.5 %s -d %s -p %s" % (fname, device_type, p1)))
+    device = subprocess.Popen(shlex.split("python3 %s -d %s -p %s" % (fname, device_type, p1)))
     popens.append(device)
     return p2
 
@@ -32,7 +32,7 @@ def cleanup():
 
 if __name__ == "__main__":
     with open("virtual_devices.txt", "w+") as device_file:
-        devices = [spawn_device("LimitSwitch"), spawn_device("ServoControl")]
+        devices = [spawn_device(device_type) for device_type in devices_to_spawn]
         device_file.write(" ".join(devices))
     while True:
         time.sleep(1)

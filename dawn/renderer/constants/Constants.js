0ecc57b3fdcd28a7fb7f37985bfd2c54836e778d
Kevin Ji <kevin.ji@outlook.com>
2017-02-20 13:23:24 -0800

    [Dawn] Constants: Use ES6 exports instead of module.exports (#137)

--- dawn/renderer/constants/Constants.js
+++ dawn/renderer/constants/Constants.js
@@ -1,21 +1,19 @@
 import keyMirror from 'keymirror';
 
-module.exports = {
-  VERSION: '0.4.0',
-  ActionTypes: keyMirror({
-    UPDATE_GAMEPADS: null,
-  }),
-  PeripheralTypes: keyMirror({
-    MOTOR_SCALAR: null,
-    SENSOR_BOOLEAN: null,
-    SENSOR_SCALAR: null,
-    LimitSwitch: null,
-    LineFollower: null,
-    Potentiometer: null,
-    Encoder: null,
-    ColorSensor: null,
-    MetalDetector: null,
-    ExampleDevice: null,
-    ServoControl: null,
-  }),
-};
+export const VERSION = '0.4.0';
+export const ActionTypes = keyMirror({
+  UPDATE_GAMEPADS: null,
+});
+export const PeripheralTypes = keyMirror({
+  MOTOR_SCALAR: null,
+  SENSOR_BOOLEAN: null,
+  SENSOR_SCALAR: null,
+  LimitSwitch: null,
+  LineFollower: null,
+  Potentiometer: null,
+  Encoder: null,
+  ColorSensor: null,
+  MetalDetector: null,
+  ExampleDevice: null,
+  ServoControl: null,
+});

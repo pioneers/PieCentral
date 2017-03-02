6953b850a0c8d7e5079e0d4a13976fe95eb6dd8c
Allan Zhou <allan.zhou@berkeley.edu>
2017-03-02 03:30:22 -0800

    Remove redux devtools (#173)
    * Remove redux devtools
    
    * Remove from packagejson
    
    * Fix lint errors

--- dawn/renderer/configureStore.js
+++ dawn/renderer/configureStore.js
@@ -2,7 +2,6 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import dawnApp from './reducers/dawnApp';
 import rootSaga from './utils/sagas';
-import DevTools from './components/DevTools';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -10,11 +9,9 @@ const store = createStore(
   dawnApp,
   compose(
     applyMiddleware(sagaMiddleware),
-    // For memory reasons, DevTools only keeps most recent {maxAge} actions
-    DevTools.instrument({ maxAge: 30 }),
   ),
 );
 
 sagaMiddleware.run(rootSaga);
 
-export { store, DevTools };
+export { store };
--- dawn/renderer/index.js
+++ dawn/renderer/index.js
@@ -3,13 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import { store, DevTools } from './configureStore';
+import { store } from './configureStore';
 
 ReactDOM.render(
   <Provider store={store}>
     <div>
       <App />
-      <DevTools />
     </div>
   </Provider>,
   document.getElementById('content'),

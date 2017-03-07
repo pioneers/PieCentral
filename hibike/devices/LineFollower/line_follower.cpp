46265b01056f786bfa24c8563caa2d10dc4c7711
tingtingchiang <tingtingchiang@users.noreply.github.com>
2017-03-06 16:58:10 -0800

    [HIBIKE] Master-ready LineFollower (#201)
    * [HIBIKE] Master-ready LineFollower
    
    * [HIBIKE] normalized data, updated method comments and year enum
    
    * [HIBIKE] fixed data_read comment and push param
    
    * [HIBIKE] Fixed casting error (int to float)

--- hibike/devices/LineFollower/line_follower.cpp
+++ hibike/devices/LineFollower/line_follower.cpp
@@ -7,7 +7,7 @@ void setup() {
 
   // Setup sensor input
   for (int i = 0; i < NUM_PINS; i++) {
-    pinMode(pins[i], INPUT_PULLUP);
+    pinMode(pins[i], INPUT);
   }
 
 }
@@ -18,44 +18,35 @@ void loop() {
 }
 
 
-uint32_t device_update(uint8_t param, uint32_t value) {
-  return ~((uint32_t) 0);
-}
-
-
-uint32_t device_status(uint8_t param) {
-  return ~((uint32_t) 0);
-}
-
-
-// you must implement this function. It is called with a buffer and a maximum buffer size.
-// The buffer should be filled with appropriate data for a DataUpdate packer, and the number of bytes
-// added to the buffer should be returned. 
+// You must implement this function.
+// It is called when the device receives a Device Write packet.
+// Updates param to new value passed in data.
+//    param   -   Parameter index
+//    data    -   value to write, in bytes, little-endian
+//    len     -   number of bytes in data
 //
-// You can use the helper function append_buf.
-// append_buf copies the specified amount data into the dst buffer and increments the offset
-uint8_t data_update(uint8_t* data_update_buf, size_t buf_len) {
-  if (buf_len < sizeof(uint16_t) * NUM_PINS) {
-    return 0;
-  }
+//   return  -   size of bytes written on success; otherwise return 0
 
-  // Read sensor
-  for (int i = 0; i < NUM_PINS; i++) {
-      data[i] = analogRead(pins[i]);  
-  }
-  
-  // Append data to packet buffer
-  uint8_t offset = 0;
-  for (int i = 0; i < NUM_PINS; i++) {
-    append_buf(data_update_buf, &offset, (uint8_t *)&data[i], sizeof(uint16_t));
-  }
-
-  return offset;
+uint32_t device_write(uint8_t param, uint8_t* data, size_t len) {
+  return 0;
 }
+
+
 
 // You must implement this function.
-// It is called when the BBB sends a message to the Smart Device tellinng the Smart Device to disable itself.
-// Consult README.md, section 6, to see what exact functionality is expected out of disable.
-void device_disable() {
+// It is called when the device receives a Device Data packet.
+// Modifies data to contain the parameter value.
+//    param           -   Parameter index
+//    data            -   buffer to return data in, little-endian
+//    len             -   length of the buffer left to write to
+//
+//    return          -   sizeof(param) on success; 0 otherwise 
 
+uint8_t device_read(uint8_t param, uint8_t* data, size_t len) {
+  if (MAX_PAYLOAD_SIZE - len < sizeof(uint8_t)|| param > 2 || param < 0) {
+    return 0;
+  }
+  float *data_float = (float *)data;
+  data_float[0] = ((float) analogRead(pins[param]))/1023;
+  return sizeof(float);
 }

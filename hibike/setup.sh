80cb18b836e5f2e1aee95f46bd830a85547451c3
tingtingchiang <tingtingchiang@users.noreply.github.com>
2017-03-21 01:10:33 -0700

    Hibike/arduino script (#276)
    * [HIBIKE] Added option to extract new Arduino library (1.8.1) and use that in the Makefile with checks to only extract and move arduino*.tar* in setup.sh
    *[HIBIKE] Added line to add to dialout group + HID and arduino-1.8.1 specific paths to Makefile

--- hibike/setup.sh
+++ hibike/setup.sh
@@ -9,6 +9,18 @@ cp utils/flash /etc/bash_completion.d/flash.sh
 echo "Setup Tab-complete"
 echo "Please restart your terminals"
 
+sudo usermod -a -G dialout $(whoami)
+
+if [ $# -eq 1 ] && [ $(echo $1 | grep -c -i -E ".*arduino.*(\.tar|\.tar\.xz)") != 0 ]; then
+    echo "Extracting Arduino file..."
+    tar xf $1 -C /opt
+    echo "Successfully installed Arduino 1.8.1 into /opt"
+elif [ $# -eq 1 ]; then
+    echo "You must download Arduino 1.8.1 from https://www.arduino.cc/en/Main/Software"
+else
+    echo "To update to Arduino 1.8.1: First command line argument as file path to .tar (or .tar.xz)"
+fi
+
 if [ "$(dpkg-query -W -f='${Status}' make 2>/dev/null | grep -c "ok installed")" != 1 ]; then
     apt-get --assume-yes install make	
 fi
@@ -41,7 +53,7 @@ if [ "$(dpkg-query -W -f='${Status}' python3-pip 2>/dev/null | grep -c "ok insta
     apt-get --assume-yes install python3-pip
 fi
 
-if [ "$(pip3 freeze | grep -c "pyserial")" != 1]; then
+if [ "$(pip3 freeze | grep -c "pyserial")" != 1 ]; then
     pip3 install -r requirements.txt
 fi
 

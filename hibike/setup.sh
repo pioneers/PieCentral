d8c623e786be45862f5e64d40f71847639520c1d
Kevin Ji <kevin.ji@outlook.com>
2017-04-05 00:36:37 -0700

    [HIBIKE] travis: Test Hibike make (#286)
    * [HIBIKE] travis: Test Hibike
    
    * [HIBIKE] travis: Remove Hibike from release branches
    
    * [HIBIKE] Makefile: Try test command
    
    * [HIBIKE] Makefile: Download Arduino for setup.sh
    
    * [HIBIKE] travis/Makefile: Remove redundant comment
    
    * [HIBIKE] setup.sh: Quote variables
    
    * [HIBIKE] travis/Makefile: Try running make on every device
    
    * [HIBIKE] travis/Makefile: Use Make variables
    
    * [HIBIKE] travis/Makefile: Explicitly enumerate working devices

--- hibike/setup.sh
+++ hibike/setup.sh
@@ -9,11 +9,11 @@ cp utils/flash /etc/bash_completion.d/flash.sh
 echo "Setup Tab-complete"
 echo "Please restart your terminals"
 
-sudo usermod -a -G dialout $(whoami)
+sudo usermod -a -G dialout "$(whoami)"
 
-if [ $# -eq 1 ] && [ $(echo $1 | grep -c -i -E ".*arduino.*(\.tar|\.tar\.xz)") != 0 ]; then
+if [ $# -eq 1 ] && [ "$(echo "$1" | grep -c -i -E ".*arduino.*(\.tar|\.tar\.xz)")" != 0 ]; then
     echo "Extracting Arduino file..."
-    tar xf $1 -C /opt
+    tar xf "$1" -C /opt
     echo "Successfully installed Arduino 1.8.1 into /opt"
 elif [ $# -eq 1 ]; then
     echo "You must download Arduino 1.8.1 from https://www.arduino.cc/en/Main/Software"
@@ -22,31 +22,31 @@ else
 fi
 
 if [ "$(dpkg-query -W -f='${Status}' make 2>/dev/null | grep -c "ok installed")" != 1 ]; then
-    apt-get --assume-yes install make	
+    apt-get --assume-yes install make
 fi
 
 if [ "$(dpkg-query -W -f='${Status}' gcc 2>/dev/null | grep -c "ok installed")" != 1 ]; then
-    apt-get --assume-yes install gcc	
+    apt-get --assume-yes install gcc
 fi
 
 if [ "$(dpkg-query -W -f='${Status}' gcc-avr 2>/dev/null | grep -c "ok installed")" != 1 ]; then
-    apt-get --assume-yes install gcc-avr	
+    apt-get --assume-yes install gcc-avr
 fi
 
 if [ "$(dpkg-query -W -f='${Status}' arduino 2>/dev/null | grep -c "ok installed")" != 1 ]; then
-    apt-get --assume-yes install arduino	
+    apt-get --assume-yes install arduino
 fi
 
 if [ "$(dpkg-query -W -f='${Status}' arduino-core 2>/dev/null | grep -c "ok installed")" != 1 ]; then
-    apt-get --assume-yes install arduino-core	
+    apt-get --assume-yes install arduino-core
 fi
 
 if [ "$(dpkg-query -W -f='${Status}' arduino-mk 2>/dev/null | grep -c "ok installed")" != 1 ]; then
-    apt-get --assume-yes install arduino-mk	
+    apt-get --assume-yes install arduino-mk
 fi
 
 if [ "$(dpkg-query -W -f='${Status}' avr-libc 2>/dev/null | grep -c "ok installed")" != 1 ]; then
-    apt-get --assume-yes install avr-libc	
+    apt-get --assume-yes install avr-libc
 fi
 
 if [ "$(dpkg-query -W -f='${Status}' python3-pip 2>/dev/null | grep -c "ok installed")" != 1 ]; then
@@ -56,6 +56,5 @@ fi
 if [ "$(pip3 freeze | grep -c "pyserial")" != 1 ]; then
     pip3 install -r requirements.txt
 fi
-
 
 echo "All dependencies installed."

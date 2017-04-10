3c3f58d201e9a467388753103e75fa6bbfd8dd8d
Kevin Ji <kevin.ji@outlook.com>
2017-04-10 16:08:17 -0700

    [DAWN] ConsoleOutput: Remove collapsibility, clean up styles (#281)
    * [DAWN] ConsoleOutput: Remove collapsibility, clean up styles
    
    * [Dawn] Console autoscrolls to bottom

--- dawn/renderer/components/ConsoleOutput.js
+++ dawn/renderer/components/ConsoleOutput.js
@@ -2,18 +2,48 @@ import React from 'react';
 import { Panel } from 'react-bootstrap';
 
 class ConsoleOutput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.scrollToBottom = this.scrollToBottom.bind(this);
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
   componentWillReceiveProps(nextProps) {
     if (this.props.output.size === 0 && nextProps.output.size > 0 && !this.props.show) {
       this.props.toggleConsole();
     }
   }
 
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    this.messagesEnd.scrollIntoView({ behavior: 'smooth' });
+  }
+
+
   render() {
     const height = `${String(this.props.height)}px`;
     return (
       <div>
-        <Panel collapsible expanded={this.props.show}>
-          <pre style={{ position: 'relative', height }}>
+        <Panel
+          style={{
+            display: this.props.show ? 'block' : 'none',
+            marginBottom: '0',
+            borderRadius: '0',
+          }}
+        >
+          <pre
+            style={{
+              position: 'relative',
+              margin: '0',
+              height,
+            }}
+          >
             <div
               style={{
                 position: 'absolute',
@@ -27,6 +57,10 @@ class ConsoleOutput extends React.Component {
               {this.props.output.map(line => (
                 <code key={`${line}-Code-${Math.random()}`}>{line}</code>
               ))}
+              <div
+                style={{ float: 'left', clear: 'both' }}
+                ref={(el) => { this.messagesEnd = el; }}
+              />
             </div>
           </pre>
         </Panel>

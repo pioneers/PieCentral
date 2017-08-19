e9fa2fecc772d4acd0d34d8bfe490ddb35b27552
Brose Johnstone <baby-bell@users.noreply.github.com>
2017-08-19 14:00:45 -0700

    [HIBIKE] More friendly flasher (#393)
    * Make the flashing script more user friendly
    
    Previously, the flashing script had no help messages and didn't check
    that its arguments were valid in any way. This commit adds some
    logic to check argument validity and a (somewhat) helpful help
    message.

--- hibike/flash.sh
+++ hibike/flash.sh
@@ -1,10 +1,56 @@
-#/bin/bash
+#!/bin/bash
+set -e
 
+NAME="$0"
+SENSOR_TYPES="$(source utils/device_names.sh)"
+
+print_usage() {
+    echo "usage: $NAME [-h, --help] SENSOR_TYPE"
+    echo "Valid sensors: $SENSOR_TYPES"
+    exit 1
+}
+
+is_valid_sensor() {
+    for sensor in "$SENSOR_TYPES"; do
+        if [[ "$0" == "$sensor" ]]; then
+            return 1
+        fi
+    done
+    return 0
+}
+
+if (( $# != 1 )); then
+    echo "Wrong number of arguments"
+    print_usage
+    exit 1
+fi
+
+# https://stackoverflow.com/questions/12022592/how-can-i-use-long-options-with-the-bash-getopts-builtin#12026302
+for arg in "$@"; do
+    shift
+    case "$arg" in
+        "--help") set -- "$@" "-h" ;;
+        "--"*) echo "Invalid option"; print_usage ;;
+        *) set -- "$@" "$arg" ;;
+    esac
+done
+
+while getopts ":h" opt; do
+    case $opt in
+        h) print_usage ;;
+        \?) echo "Invalid option"; print_usage ;;
+    esac
+done
+
+if [ ! "$(is_valid_sensor "$1")" ]; then
+    echo "Invalid sensor type: $1"
+    exit 1
+fi
 #make clean doesn't actually work.  Therefore,
 
 #move to wheverever the flash script is run, 
 #and then (as long i've successfuly moved), remove the compiled bin file.
-cd `dirname $0` && rm -rf ./bin
+cd "$(dirname "$0")" && rm -rf ./bin
 
 
 python reset.py /dev/ttyACM*
@@ -17,4 +63,4 @@ sleep 0.5
 # /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C/Users/bduckering/Library/Arduino15/packages/SparkFun/hardware/avr/1.0.3/avrdude.conf -v -patmega32u4 -cavr109 -P/dev/cu.usbmodem1421 -b57600 -D -Uflash:w:"$1":i
 # avrdude -v -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D -Uflash:w:"$1":i
 
-make upload DEVICE=$1
+make upload DEVICE="$1"
--- hibike/utils/device_names.sh
+++ hibike/utils/device_names.sh
@@ -5,6 +5,6 @@
 
 #this function is not meant to be called from the "utils" folder.  It will fail because of pathing problems.
 #it is meant to be called from the "hibike" folder, since that is where flash.sh is located.
-
-
-ls devices/
+set -e
+cd devices/
+echo *

1a68c394d969ff592b1d30bdd6f93608c4e3f733
Brose Johnstone <baby-bell@users.noreply.github.com>
2017-09-23 14:10:45 -0700

    [HIBIKE] Remove reset.py, fix flashing script
    The reset code has broken, and it hasn't been needed for quite a while now. Flashing works without it
    as long as you run setup.sh.

--- hibike/flash.sh
+++ hibike/flash.sh
@@ -50,12 +50,6 @@ check_sensor $1
 #and then (as long i've successfuly moved), remove the compiled bin file.
 cd "$(dirname "$0")" && rm -rf ./bin
 
-python reset.py /dev/ttyACM*
-sleep 0.5
-python reset.py /dev/ttyACM*
-sleep 0.5
-python reset.py /dev/ttyACM*
-sleep 0.5
 # echo "device is:  $1"
 # /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C/Users/bduckering/Library/Arduino15/packages/SparkFun/hardware/avr/1.0.3/avrdude.conf -v -patmega32u4 -cavr109 -P/dev/cu.usbmodem1421 -b57600 -D -Uflash:w:"$1":i
 # avrdude -v -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D -Uflash:w:"$1":i

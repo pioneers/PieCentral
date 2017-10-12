5356f116e6f0a1667d3ff943c3e03d0e4940be42
Brandon Lee <brandonxxlee@gmail.com>
2017-10-11 20:23:51 -0700

    [SHEPHERD] Add API for Timer (#421)

--- shepherd/Timer.py
+++ shepherd/Timer.py
@@ -1,26 +1,21 @@
 class Timer:
-    '''
+    """
     This class should spawn another thread that will keep track of a target time
     and compare it to the current system time in order to see how much time is left
-    '''
-    def __init__(self):
-        pass
+    """
+    def __init__(self, timer_type, goal_name=None):
+        self.active = False
+        self.timer_type = timer_type
+        self.goal_name = goal_name
 
     def start_timer(self, duration):
-        '''
-        Starts a new timer with the duration which is given in seconds
-        '''
+        """Starts a new timer with the duration (seconds) and sets timer to active"""
         pass
 
-    def extend_duration(self, duration):
-        '''
-        Adds time to the timer based on the duration which is given in seconds
-        '''
+    def reset(self):
+        """Stops the current timer (if any) and sets timer to inactive"""
         pass
 
-    def get_time_left(self):
-        '''
-        Returns the amount of time left in seconds
-        Returns -1 if the most recent timer has expired/there is no timer being tracked
-        '''
-        pass
+    def is_running(self):
+        """Returns true if the timer is currently running"""
+        return self.active

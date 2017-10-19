c3fe78ad23d57a5fd10c7a8b3c27c563297b338e
Tiffany Chiang <tingtingchiang@users.noreply.github.com>
2017-10-18 21:04:35 -0700

    [HIBIKE] removed hibike_simulation from hibike/__init__.py (#435)

--- hibike/__init__.py
+++ hibike/__init__.py
@@ -1,6 +1,5 @@
 """
 Hibike is a communications protocol. See README.md for more info.
 """
-__all__ = ['hibike_simulator', 'hibike_message']
-from . import hibike_simulator
+__all__ = ['hibike_message']
 from . import hibike_message

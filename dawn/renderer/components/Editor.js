11f98419d6f61fff7e8b5139f6bebdd171cea79a
Arjun Sridhar <arjunsridhar1@gmail.com>
2017-10-28 13:29:33 -0700

    Hardcoded PiE API and some useful Python3 functions (#437)

--- dawn/renderer/components/Editor.js
+++ dawn/renderer/components/Editor.js
@@ -97,7 +97,59 @@ class Editor extends React.Component {
    * Confirmation Dialog on Quit, Stored Editor Settings, Window Size-Editor Re-render
    */
   componentDidMount() {
-    this.CodeEditor.editor.setOption('enableBasicAutocompletion', true);
+    this.CodeEditor.editor.setOptions({
+      enableBasicAutocompletion: true,
+      enableSnippets: true,
+      enableLiveAutocompletion: true,
+    });
+    const autoComplete = {
+      getCompletions(editor, session, pos, prefix, callback) {
+        callback(null, [
+          { value: 'Robot', score: 1000, meta: 'PiE API' },
+          { value: 'get_value', score: 900, meta: 'PiE API' },
+          { value: 'set_value', score: 900, meta: 'PiE API' },
+          { value: 'run', score: 900, meta: 'PiE API' },
+          { value: 'is_running', score: 900, meta: 'PiE API' },
+          { value: 'Gamepad', score: 1000, meta: 'PiE API' },
+          { value: 'Actions', score: 1000, meta: 'PiE API' },
+          { value: 'sleep', score: 900, meta: 'PiE API' },
+          { value: '"button_a"', score: 900, meta: 'PiE API' },
+          { value: '"button_b"', score: 900, meta: 'PiE API' },
+          { value: '"button_x"', score: 900, meta: 'PiE API' },
+          { value: '"button_y"', score: 900, meta: 'PiE API' },
+          { value: '"l_bumper"', score: 900, meta: 'PiE API' },
+          { value: '"r_bumper"', score: 900, meta: 'PiE API' },
+          { value: '"l_trigger"', score: 900, meta: 'PiE API' },
+          { value: '"r_trigger"', score: 900, meta: 'PiE API' },
+          { value: '"button_back"', score: 900, meta: 'PiE API' },
+          { value: '"button_start"', score: 900, meta: 'PiE API' },
+          { value: '"l_stick"', score: 900, meta: 'PiE API' },
+          { value: '"r_stick"', score: 900, meta: 'PiE API' },
+          { value: '"dpad_up"', score: 900, meta: 'PiE API' },
+          { value: '"dpad_down"', score: 900, meta: 'PiE API' },
+          { value: '"dpad_left"', score: 900, meta: 'PiE API' },
+          { value: '"dpad_right"', score: 900, meta: 'PiE API' },
+          { value: '"button_xbox"', score: 900, meta: 'PiE API' },
+          { value: 'def', score: 1000, meta: 'Python3' },
+          { value: 'await', score: 1000, meta: 'Python3' },
+          { value: 'print', score: 1000, meta: 'Python3' },
+          { value: 'max', score: 1000, meta: 'Python3' },
+          { value: 'min', score: 1000, meta: 'Python3' },
+          { value: 'async', score: 1000, meta: 'Python3' },
+          { value: 'lamda', score: 1000, meta: 'Python3' },
+          { value: 'for', score: 1000, meta: 'Python3' },
+          { value: 'while', score: 1000, meta: 'Python3' },
+          { value: 'True', score: 1000, meta: 'Python3' },
+          { value: 'False', score: 1000, meta: 'Python3' },
+          { value: 'abs', score: 1000, meta: 'Python3' },
+          { value: 'len', score: 1000, meta: 'Python3' },
+          { value: 'round', score: 1000, meta: 'Python3' },
+          { value: 'set()', score: 1000, meta: 'Python3' },
+        ]);
+      },
+    };
+    this.CodeEditor.editor.completers = [autoComplete];
+
     this.onWindowResize();
     storage.get('editorTheme', (err, data) => {
       if (err) {

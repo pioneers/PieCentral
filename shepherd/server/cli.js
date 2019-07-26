8f370e34762ffe4401d1cf4bf65af24a4bd64fb6
Jonathan Lee <jonathan-lee@berkeley.edu>
2019-07-26 00:06:02 -0700

    Added basic structured logging

--- shepherd/server/cli.js
+++ shepherd/server/cli.js
@@ -1,20 +1,70 @@
 const { Command, flags } = require('@oclif/command');
+const winston = require('winston');
+const app = require('./views');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'shepherd' }
+});
 
 class ServerCommand extends Command {
+  configureLogger(flags) {
+    switch (flags.mode) {
+      case 'production':
+        logger.add(new winston.transports.File({
+          filename: flags.log
+        }));
+        break;
+      default:
+        logger.add(new winston.transports.Console({
+          level: 'debug',
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.printf(info => `${new Date().toISOString()} [${info.level}]: ${info.message}`)
+          )
+        }));
+        break;
+    }
+  }
+
   async run() {
     const { flags } = this.parse(ServerCommand);
-    let port = flags.port;
-    console.log(port);
+    this.configureLogger(flags);
+    logger.debug('Logging initialized');
+    app.listen(flags.port, flags.host, () => {
+      logger.info(`Server listening on ${flags.host}:${flags.port}`, {
+        host: flags.host,
+        port: flags.port,
+      });
+    });
   }
 }
 
 ServerCommand.flags = {
   version: flags.version({ char: 'v' }),
   help: flags.help({ char: 'h' }),
+  host: flags.string({
+    char: 'n',
+    description: 'Host to bind the server to',
+    default: '127.0.0.1',
+  }),
   port: flags.string({
     char: 'p',
-    description: 'Port to run the server on',
+    description: 'Port to bind the server to',
     default: 6060,
+  }),
+  log: flags.string({
+    char: 'l',
+    description: 'Log path',
+    default: 'log/aggregate.log',
+  }),
+  mode: flags.string({
+    mode: 'm',
+    env: 'NODE_ENV',
+    default: 'development',
+    description: 'Mode',
+    options: ['development', 'production'],
   }),
 };
 
--- shepherd/webpack.config.js
+++ shepherd/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app/app.js',
+  entry: './client/app.js',
   module: {
     rules: [{
       test: /\.(js|jsx)$/i,

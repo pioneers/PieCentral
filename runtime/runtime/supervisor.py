e1f00046077ebb271495c05bc06685d9441a34e4
Jonathan Lee <jonathan-lee@berkeley.edu>
2019-10-30 20:23:20 -0700

    Added starting SDK API
    Prepared Nodejs to Runtime SDK methods
    
    [Runtime] Added the toByteArray method, and changed socket type to request from pair
    
    [Runtime] Implemented basic RPC protocal and RuntimeClient methods.
    
    [Runtime] Added Documentation to methods in RuntimeClient
    
    [RUNTIME] implemented runtime client with ZMQ 6.0.0

--- runtime/runtime/supervisor.py
+++ runtime/runtime/supervisor.py
@@ -19,58 +19,104 @@ import zmq.asyncio
 from runtime import get_version
 from runtime.service import SERVICES
 from runtime.service.base import Service
-from runtime.monitoring.retry import Proxy
+from runtime.monitoring import retry
 from runtime.util.exception import EmergencyStopException
 
 LOGGER = structlog.get_logger()
 
 
 @dataclasses.dataclass
-class ServiceSupervisor:
-    subprocesses: Mapping[int, Any] = dataclasses.field(default_factory=dict)
+class Subprocess(retry.Proxy):
+    name: str
+    service_config: dict
+    terminate_timeout: int = 2
 
+    def initialize_resource(self):
+        service = self.service_base()
+        subprocess = aioprocessing.AioProcess(
+            name=name,
+            target=self.service_base(),
+            **self.service_config,
+        )
+        subprocess.start()
+        return subprocess
+
+    def terminate_resource(self, subprocess):
+        context = {'name': subprocess.name, 'pid': subprocess.pid}
+        subprocess.terminate()
+        LOGGER.warn('Sent SIGTERM to subprocess', **context,
+                    terminate_timeout=self.terminate_timeout)
+        subprocess.join(self.terminate_timeout)
+        time.sleep(0.05)  # Allow `exitcode` to set.
+
+        if subprocess.is_alive():
+            subprocess.kill()
+            LOGGER.critical('Sent SIGKILL to subprocess '
+                            '(unable to terminate gracefully)', **context)
+        else:
+            LOGGER.debug('Subprocess terminated cleanly', **context)
+
+    async def spin(self):
+        LOGGER.info('Starting service ...')
+
+
+class SubprocessSupervisor:
     def __enter__(self):
         return self
 
     def __exit__(self, *_):
-        for subprocess in self.subprocesses.values():
-            self.terminate(subprocess)
+        pass
 
-    @backoff.on_predicate(backoff.constant, interval=1, max_tries=5, logger=LOGGER)
-    async def spawn(self, name: str, service: Service, *args, **kwargs):
-        subprocess = aioprocessing.AioProcess(name=name, target=service, *args,
-                                              **kwargs, daemon=True)
-        subprocess.start()
-        self.subprocesses[subprocess.pid] = subprocess
-        LOGGER.debug('Started subprocess', name=name)
-        await subprocess.coro_join()
-        if subprocess.exitcode == EmergencyStopException.EXIT_CODE:
-            LOGGER.critical('Received emergency stop', name=name)
-            raise EmergencyStopException()
-        self.terminate(subprocess)
-        del self.subprocesses[subprocess.pid]
+    async def spin(self, config):
+        print(config)
 
-    def terminate(self, subprocess, terminate_timeout=2):
-        subprocess.terminate()
-        LOGGER.warn('Sent SIGTERM to subprocess', name=subprocess.name)
-        subprocess.join(terminate_timeout)
-        time.sleep(0.05)  # Allow "exitcode" to set.
-        if subprocess.is_alive():
-            subprocess.kill()
-            LOGGER.critical('Sent SIGKILL to subprocess, unabel to shut down gracefully',
-                            name=name, timeout=terminate_timeout)
-        else:
-            LOGGER.debug('Subprocess terminated cleanly', name=subprocess.name)
 
-    async def spin(self, **services):
-        LOGGER.info('Starting services ...', services=list(services.keys()))
-        subprocesses = []
-        for name, config in services.items():
-            replicas = config.get('replicas', 1)
-            for replica in range(replicas):
-                service = SERVICES[name]()
-                subprocesses.append(self.spawn(f'{name}-{replica}', service, **config))
-        await asyncio.wait(subprocesses, return_when=asyncio.FIRST_COMPLETED)
+# @dataclasses.dataclass
+# class ServiceSupervisor:
+#     subprocesses: Mapping[int, Any] = dataclasses.field(default_factory=dict)
+#
+#     def __enter__(self):
+#         return self
+#
+#     def __exit__(self, *_):
+#         for subprocess in self.subprocesses.values():
+#             self.terminate(subprocess)
+#
+#     @backoff.on_predicate(backoff.constant, interval=1, max_tries=5, logger=LOGGER)
+#     async def spawn(self, name: str, service: Service, *args, **kwargs):
+#         subprocess = aioprocessing.AioProcess(name=name, target=service, *args,
+#                                               **kwargs, daemon=True)
+#         subprocess.start()
+#         self.subprocesses[subprocess.pid] = subprocess
+#         LOGGER.debug('Started subprocess', name=name)
+#         await subprocess.coro_join()
+#         if subprocess.exitcode == EmergencyStopException.EXIT_CODE:
+#             LOGGER.critical('Received emergency stop', name=name)
+#             raise EmergencyStopException()
+#         self.terminate(subprocess)
+#         del self.subprocesses[subprocess.pid]
+#
+#     def terminate(self, subprocess, terminate_timeout=2):
+#         subprocess.terminate()
+#         LOGGER.warn('Sent SIGTERM to subprocess', name=subprocess.name)
+#         subprocess.join(terminate_timeout)
+#         time.sleep(0.05)  # Allow "exitcode" to set.
+#         if subprocess.is_alive():
+#             subprocess.kill()
+#             LOGGER.critical('Sent SIGKILL to subprocess, unabel to shut down gracefully',
+#                             name=name, timeout=terminate_timeout)
+#         else:
+#             LOGGER.debug('Subprocess terminated cleanly', name=subprocess.name)
+#
+#     async def spin(self, **services):
+#         LOGGER.info('Starting services ...', services=list(services.keys()))
+#         subprocesses = []
+#         for name, config in services.items():
+#             replicas = config.get('replicas', 1)
+#             for replica in range(replicas):
+#                 service = SERVICES[name]()
+#                 subprocesses.append(self.spawn(f'{name}-{replica}', service, **config))
+#         await asyncio.wait(subprocesses, return_when=asyncio.FIRST_COMPLETED)
 
 
 @dataclasses.dataclass
@@ -125,8 +171,8 @@ class Runtime:
                 config = yaml.load(await config_file.read())
             LOGGER.debug(f'Read configuration from disk', path=self.config_file)
 
-            with ServiceSupervisor() as supervisor:
-                await supervisor.spin(**config['services'])
+            with SubprocessSupervisor() as supervisor:
+                await supervisor.spin(config['services'])
         except Exception as exc:
             LOGGER.critical('Error reached the top of the call stack')
             raise exc

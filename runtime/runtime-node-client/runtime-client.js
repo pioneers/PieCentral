f817b7963644d21181ff1c42658c69d64cc06e91
Jonathan Lee <jonathan-lee@berkeley.edu>
2019-10-30 20:23:20 -0700

    Added starting SDK API
    Prepared Nodejs to Runtime SDK methods
    
    [Runtime] Added the toByteArray method, and changed socket type to request from pair
    
    [Runtime] Implemented basic RPC protocal and RuntimeClient methods.
    
    [Runtime] Added Documentation to methods in RuntimeClient
    
    [RUNTIME] implemented runtime client with ZMQ 6.0.0

--- runtime/runtime-node-client/runtime-client.js
+++ runtime/runtime-node-client/runtime-client.js
@@ -1,9 +1,201 @@
-const zmq = require('zeromq');
+const zmq = require('zeromq'),
+  msgpack = require('@ygoe/msgpack');
 
+//This is in case we change the protocol for some reason
+const type = {
+    request: 0,
+    reply: 1,
+    notification: 2
+};
+
+/**
+ *  An implementation of a client connection to
+ *  a runtime server, using zeromq and msgpack.
+ */
 class RuntimeClient {
-  constructor() {
+
+  /**
+   * 
+   * @param {string} host - IP address of Runtime (ex: 'tcp://127.0.0.1:3000') 
+   */
+  constructor(host) {
+    this.socket = new zmq.Request;
+    this.msgid = 0;
+    try {
+      this.socket.connect(host);
+      console.log('Client NodeJS to Runtime port connected');
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  /**
+   * Set the robot's team alliance color
+   * @param {string} alliance - should be 'blue', 'gold', or 'none'
+   */
+  setAlliance(alliance) {
+    let msg = new RequestMessage(this.msgid, 'setAlliance', [alliance]);
+    this.msgid++;
+    this.socket.send(msg.toSocketMessage());
+    const [msg] = await this.socket.receive();
+    const decodedMsg =  msgpack.deserialize(msg);
+    const err = decodedMsg[2];
+    if(err) {
+      throw err;
+    } else {
+      return;
+    }
+  }
+
+  /**
+   * Set the robot's current operating mode
+   * @param {string} mode - should be 'auto', 'teleop', 'idle', or 'estop'
+   */
+  setMode(mode) {
+    let msg = new RequestMessage(this.msgid, 'setMode', [mode]);
+    this.msgid++;
+    this.socket.send(msg.toSocketMessage());
+    const [msg] = await this.socket.receive();
+    const decodedMsg =  msgpack.deserialize(msg);
+    const err = decodedMsg[2];
+    if(err) {
+      throw err;
+    } else {
+      return;
+    }
+  }
+
+  /**
+   * Set the starting zone of the robot
+   * @param {string} zone - should be 'left', 'right', or 'none'
+   */
+  setStartingZone(zone) {
+    let msg = new RequestMessage(this.msgid, 'setStartingZone', [zone]);
+    this.msgid++;
+    this.socket.send(msg.toSocketMessage());
+    const [msg] = await this.socket.receive();
+    const decodedMsg =  msgpack.deserialize(msg);
+    const err = decodedMsg[2];
+    if(err) {
+      throw err;
+    } else {
+      return;
+    }
+  }
+
+  /**
+   * Run the robot's coding challenge solution
+   * @param {int} seed - the seed for the challenge
+   * @param {int} timeout - how long to wait for the code to finish 
+   */
+  runCodingChallenge(seed, timeout = 'something') {
+    let msg = new RequestMessage(this.msgid, 'runCodingChallenge', [seed, timeout]);
+    this.msgid++;
+    this.socket.send(msg.toSocketMessage());
+    const [msg] = await this.socket.receive();
+    const decodedMsg =  msgpack.deserialize(msg);
+    const returnVal = decodedMsg[3];
+    if(returnVal != null) {
+      return returnVal;
+    } else {
+      throw decodedMsg[2];
+    }
+  }
+
+  /**
+   * Get a mapping of UIDs to names for all devices
+   * on the robot
+   */
+  listDeviceNames() {
+    let msg = new RequestMessage(this.msgid, 'listDeviceNames', []);
+    this.msgid++;
+    this.socket.send(msg.toSocketMessage());
+    const [msg] = await this.socket.receive();
+    const decodedMsg =  msgpack.deserialize(msg);
+    const returnVal = decodedMsg[3];
+    if(returnVal != null) {
+      return returnVal;
+    } else {
+      throw decodedMsg[2];
+    }
+  }
+
+  /**
+   * Set the name of the device with the passed in UID
+   * @param {string} uid - the UID of the device you want to name
+   * @param {string} name - the desired name of the device
+   */
+  setDeviceName(uid, name) {
+    let msg = new RequestMessage(this.msgid, 'setDeviceName', [uid, name]);
+    this.msgid++;
+    this.socket.send(msg.toSocketMessage());
+    const [msg] = await this.socket.receive();
+    const decodedMsg =  msgpack.deserialize(msg);
+    const err = decodedMsg[2];
+    if(err) {
+      throw err;
+    } else {
+      return;
+    }
+  }
+
+  /**
+   * Delete a device with the given UID
+   * @param {string} uid - the UID of the device to be deleted
+   */
+  delDeviceName(uid) {
+    let msg = new RequestMessage(this.msgid, 'delDeviceName', [uid]);
+    this.msgid++;
+    this.socket.send(msg.toSocketMessage());
+    const [msg] = await this.socket.receive();
+    const decodedMsg =  msgpack.deserialize(msg);
+    const err = decodedMsg[2];
+    if(err) {
+      throw err;
+    } else {
+      return;
+    }
   }
 }
 
-client = new RuntimeClient();
+/**
+ * A representation of a request
+ * message to runtime. Should NOT 
+ * be used
+ */
+class RequestMessage {
+  constructor(msgid, method, params) {
+    this.msg = [type.request, msgid, method, params];
+  }
+
+  toSocketMessage() {
+    return msgpack.serialize(this.msg);
+  }
+
+  toString() {
+    return this.msg.toString();
+  }
+
+  type() {
+    return this.msg[0];
+  }
+
+  msgid() {
+    return this.msg[1];
+  }
+
+  method() {
+    return this.msg[2];
+  }
+
+  params() {
+    return this.msg[3];
+  }
+}
+
+
+
+
+//The hostname used during testing, likely to change
+client = new RuntimeClient('tcp://127.0.0.1:3000');
 console.log(client);
--- runtime/runtime/monitoring/retry.py
+++ runtime/runtime/monitoring/retry.py
@@ -2,110 +2,48 @@
 Retryable proxies.
 """
 
+import abc
 import asyncio
 import collections
 import dataclasses
 import functools
 import inspect
-from typing import Any, Callable, Mapping
+from typing import Any, Callable, Generic, Mapping, Optional, TypeVar
 
 import backoff
 import cachetools
 from cachetools.keys import hashkey
 import structlog
 
-
-Policies = Mapping[str, Callable]
+Resource = TypeVar('Resource')
 
 
 @dataclasses.dataclass
-class Proxy:
-    """
-    Example::
+class Proxy(abc.ABC, Generic[Resource]):
+    policies: Mapping[str, Callable]
+    resource: Optional[Resource] = dataclasses.field(init=False)
 
-        >>> import logging
-        >>> fail_counter = 0
-        >>> class Resource:
-        ...     def add(self, a, b):
-        ...         return a + b
-        ...     def sub(self, a, b):
-        ...         global fail_counter
-        ...         fail_counter += 1
-        ...         if fail_counter <= 2:
-        ...             raise ValueError('transient failure')
-        ...         return a - b
-        ...     def mul(self, a, b):
-        ...         raise ValueError('invalid operation')
-        >>> policy = functools.partial(backoff.on_exception, backoff.constant,
-        ...                            ValueError, max_tries=3, interval=0)
-        >>> policies = {'sub': policy, 'mul': policy}
-        >>> resource = Proxy(Resource, logging.getLogger(), policies)
-        >>> resource.add(1, 2)
-        3
-        >>> resource.sub(1, 2)
-        -1
-        >>> resource.mul(1, 2)
-        Traceback (most recent call last):
-          ...
-        ValueError: invalid operation
-        >>> resource.div
-        Traceback (most recent call last):
-          ...
-        AttributeError: 'Resource' object has no attribute 'div'
-    """
-    factory: Callable
-    logger: structlog.BoundLoggerBase = dataclasses.field(default_factory=structlog.get_logger)
-    policies: Policies = dataclasses.field(default_factory=collections.defaultdict)
-    cleanup: Callable = lambda _resource: None
-    resource: Any = dataclasses.field(default=None, init=False)
+    def __post_init__(self):
+        for name, policy in self.policies.items():
+            method = getattr(self, name)
+            setattr(self, name, policy(method, on_backoff=self.retry_resource))
 
-    @property
-    def _resource(self):
-        """ Get or create an instance of the resource. """
-        if self.resource is None:
-            self.resource = self.factory()
-        return self.resource
+    def __enter__(self):
+        self.resource = self.initialize_resource()
+        return self
 
-    def _clear_resource(self, _debug=None):
-        """ Clean up the resource and allow a new one to be created. """
+    def __exit__(self, *_):
+        self.terminate_resource(self.resource)
+
+    @abc.abstractmethod
+    def initialize_resource(self) -> Resource:
+        pass
+
+    @abc.abstractmethod
+    def terminate_resource(self, resource: Resource):
+        pass
+
+    def retry_resource(self):
         if self.resource is not None:
-            self.cleanup(self.resource)
-        self.resource = None
-
-    @cachetools.cached(cache={}, key=lambda _, name: hashkey(name))
-    def _wrap_method(self, name: str):
-        """
-        Make a retryable version of a method on the resource.
-
-        Note::
-            The method, once built, is cached by its name.
-        """
-        policy = self.policies[name]
-        method = getattr(self._resource, name)
-        if not policy:
-            return method
-        policy = policy(logger=self.logger, on_backoff=self._clear_resource)
-
-        if inspect.iscoroutinefunction(method):
-            @policy
-            @functools.wraps(method)
-            async def wrapper(*args, **kwargs):
-                method = getattr(self._resource, name)
-                return await method(*args, **kwargs)
-        else:
-            @policy
-            @functools.wraps(method)
-            def wrapper(*args, **kwargs):
-                method = getattr(self._resource, name)
-                return method(*args, **kwargs)
-        return wrapper
-
-    def __getattr__(self, name: str):
-        # Methods on `RetryProxy` itself have priority in the lookup.
-        if name in self.__dict__:
-            return self.__dict__[name]
-        attr = getattr(self._resource, name)
-        if not inspect.ismethod(attr):
-            return attr
-        else:
-            return self._wrap_method(name)
+            self.terminate_resource(self.resource)
+        self.resource = self.initialize_resource()

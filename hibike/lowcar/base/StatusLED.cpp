4d379d27278a727a53eaea1d48999d647b0d84f8
Ben Liao <benliao1@berkeley.edu>
2019-11-10 01:01:14 -0800

    [Hibike] Lowcar base class first draft done, no tests

--- hibike/lowcar/base/StatusLED.cpp
+++ hibike/lowcar/base/StatusLED.cpp
@@ -3,12 +3,12 @@
 StatusLED::StatusLED ()
 {
 	pinMode(LED_PIN, OUTPUT);
-	digitalWrite(LED_PIN, lOW);
+	digitalWrite(LED_PIN, LOW);
 	led_enabled = false;
 }
 
 StatusLED::toggle ()
 {
-	(led_enabled) ? digitalWrite(LED_PIN, false) : digitalWrite(LED_PIN, true);
+	(led_enabled) ? digitalWrite(LED_PIN, LOW) : digitalWrite(LED_PIN, HIGH);
 	led_enabled = !led_enabled;
 }

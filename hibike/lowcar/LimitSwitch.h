13a50ea3dab0a124a69c575a91a50ccef6636b00
PiE Developer <software@pioneers.berkeley.edu>
2019-11-13 21:05:06 -0800

    added Limit Switch into new lowcar hibike rewrite

---
+++ hibike/lowcar/LimitSwitch.h
@@ -0,0 +1,23 @@
+#ifndef LIMIT_SWITCH_H
+#define LIMIT_SWITCH_H
+
+#include "Device.h"
+#include "defs.h"
+
+#define NUM_SWITCHES 3 //number of pins used on device
+
+class LimitSwitch : public Device
+{
+public:
+	//constructs a LimitSwitch; simply calls generic Device constructor with device type and year
+	LimitSwitch ();
+	
+	//overridden functions from Device class; see descriptions in Device.h
+	virtual uint8_t device_read (uint8_t param, uint8_t *data_buf, size_t data_buf_len);
+	virtual void device_enable ();
+	
+private:
+	uint8_t pins[NUM_SWITCHES] = {IN_0, IN_1, IN_2}; //pins that the line follower reads data from (defined in defs.h)
+};
+
+#endif

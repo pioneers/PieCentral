415615d7f3e9b755e363e60d6b19c633484f24c3
Ben Liao <benliao1@berkeley.edu>
2019-11-17 01:32:12 -0800

    [LOWCAR] finished changing code to C++ class convention

--- hibike/lowcar/ServoControl.h
+++ hibike/lowcar/ServoControl.h
@@ -5,14 +5,6 @@
 #include "defs.h"
 #include <Servo.h>
 
-#define NUM_PINS 2
-
-#define SERVO_0 5
-#define SERVO_1 6
-
-#define SERVO_RANGE 1000
-#define SERVO_CENTER 1500
-
 class ServoControl : public Device
 {
 public:

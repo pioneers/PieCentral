51a6772ac8778e927b5555019acd1897968d8acf
Ben Liao <benliao1@berkeley.edu>
2019-11-17 01:38:18 -0800

    [LOWCAR] finished changing code to C++ class convention servo control

--- hibike/lowcar/ServoControl.h
+++ hibike/lowcar/ServoControl.h
@@ -7,25 +7,27 @@
 
 class ServoControl : public Device
 {
-public:
-    ServoControl();
-    // Overridden functions
-    // Comments/descriptions can be found in source file
-    virtual uint8_t device_read (uint8_t param, uint8_t *data_buf, size_t data_buf_len);
-    virtual uint32_t device_write (uint8_t param, uint8_t *data_buf);
-    virtual void device_disable (); //calls helper disableAll() to detach all servos
+public:	
+	//Constructor
+	ServoControl();
+	
+	//Overridden functions
+	//Comments/descriptions can be found in source file and in Device.h
+	virtual uint8_t device_read (uint8_t param, uint8_t *data_buf, size_t data_buf_len);
+	virtual uint32_t device_write (uint8_t param, uint8_t *data_buf);
+	virtual void device_disable (); //calls helper disableAll() to detach all servos
 
 private:
 	//class constants and variables
 	const static int NUM_SERVOS, SERVO_0, SERVO_1, SERVO_CENTER, SERVO_RANGE;
-    const static uint8_t pins[ServoControl::NUM_PINS];
+	const static uint8_t pins[ServoControl::NUM_PINS];
 	
 	Servo servo0, servo1;
 	Servo servos[LimitSwitch::NUM_SERVOS];
-    float positions[LimitSwitch::NUM_SERVOS];
+	float positions[LimitSwitch::NUM_SERVOS];
 	
-    //detaches all servos
-    void disableAll ();
+	//detaches all servos
+	void disable_all ();
 };
 
 #endif
--- hibike/lowcar/ServoControl/ServoControl.cpp
+++ hibike/lowcar/ServoControl/ServoControl.cpp
@@ -11,7 +11,7 @@ const static uint8_t ServoControl::pins[ServoControl::NUM_SERVOS] = {ServoContro
 //initializes this->servos[] to contain references to the two Servo objects in initializer list
 ServoControl::ServoControl() : Device(DeviceID::SERVO_CONTROL, 1), servos(this->servo0, this->servo1)
 {
-    disableAll();
+	disable_all();
 }
 
 //Device functions
@@ -20,12 +20,12 @@ ServoControl::ServoControl() : Device(DeviceID::SERVO_CONTROL, 1), servos(this->
 //Returns size of float if successful and 0 otherwise
 uint8_t ServoControl::device_read (uint8_t param, uint8_t *data_buf, size_t data_buf_len)
 {
-    if (data_buf_len < sizeof(float)) {
+	if (data_buf_len < sizeof(float)) {
 		return 0;
 	}
-    float *float_buf = (float *) data_buf;
-    float_buf[0] = this->positions[param];
-    return sizeof(float);
+	float *float_buf = (float *) data_buf;
+	float_buf[0] = this->positions[param];
+	return sizeof(float);
 }
 
 //Attaches servo at param to corresponding pin if not attached yet
@@ -34,8 +34,8 @@ uint8_t ServoControl::device_read (uint8_t param, uint8_t *data_buf, size_t data
 //Returns size of bytes written if successful and 0 otherwise
 uint32_t ServoControl::device_write (uint8_t param, uint8_t *data_buf)
 {
-    float value = ((float *) data_buf)[0];
-    if (value < -1 || value > 1) {
+	float value = ((float *) data_buf)[0];
+	if (value < -1 || value > 1) {
 		return sizeof(float);
 	}
 	
@@ -43,22 +43,22 @@ uint32_t ServoControl::device_write (uint8_t param, uint8_t *data_buf)
 	if (!this->servos[param].attached()) {
 		this->servos[param].attach(this->pins[param]);
 	}
-    this->positions[param] = value;
-    this->servos[param].writeMicroseconds(ServoControl::SERVO_CENTER + (this->positions[param] * ServoControl::SERVO_RANGE / 2));
-    return sizeof(float);
+	this->positions[param] = value;
+	this->servos[param].writeMicroseconds(ServoControl::SERVO_CENTER + (this->positions[param] * ServoControl::SERVO_RANGE / 2));
+	return sizeof(float);
 }
 
 //Disables all the servos on call
 void ServoControl::device_disable()
 {
-    disableAll();
+	disable_all();
 }
 
 //Helper Functions
 
 //disables all servos by detaching them
 void ServoControl::disable_all() {
-    for (int i = 0; i < ServoControl::NUM_PINS; i++) {
-        this->servos[i].detach();
-    }
+	for (int i = 0; i < ServoControl::NUM_PINS; i++) {
+		this->servos[i].detach();
+	}
 }

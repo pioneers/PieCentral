406903cc95f24b850b2f6837fcd002cb2067241d
carlchua <59255048+carlchua@users.noreply.github.com>
2020-02-15 14:42:38 -0800

    recipe times v1 (#734)
    * recipe times v1
    
    * fixed style errors

--- shepherd/Alliance.py
+++ shepherd/Alliance.py
@@ -12,6 +12,9 @@ class Alliance:
             team_1_number - Integer representing team number of first team
             team_2_number - Integer representing team number of second team
             score - Integer tracking the score of the Alliance
+            recipe_times - Array storing the times for each recipe
+            recipe_count - Integer representing the number of recipes
+            penalties - Array storing the times for each penalty an alliance receives
     """
 
     def __init__(self, name, team_1_name, team_1_number, team_2_name,
@@ -26,6 +29,9 @@ class Alliance:
         self.team_2_connection = False
         self.team_1_custom_ip = team_1_custom_ip
         self.team_2_custom_ip = team_2_custom_ip
+        self.recipe_times = []
+        self.recipe_count = len(self.recipe_times)
+        self.penalties = []
 
     def change_score(self, amount):
         """ changes score of this alliance by Amount,
@@ -44,8 +50,20 @@ class Alliance:
         #TODO: Send info to sensors about reset
         #TODO: Send info to UI about reset
         #TODO: Move score sends to shepherd.py
+        self.recipe_times = []
+        self.recipe_count = 0
+        self.penalties = []
 
     def __str__(self):
         return ("<alliance: " + self.name + "> <teams: " + self.team_1_name + " " +
                 str(self.team_1_number) + ", " + self.team_2_name + " " + str(self.team_2_number) +
                 "> <score: " + str(self.score) + ">")
+
+    def increment(self, _time):
+        #TODO: Add the time to the recipe_times
+        self.recipe_times.append(_time)
+        self.recipe_count += 1
+
+    def penalty(self, _time):
+        #TODO: Add penalites
+        self.penalties.append(_time)

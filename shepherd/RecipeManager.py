58a65cb84552fcdbb50ce596cd398c12032369b2
Rusy <rusy@berkeley.edu>
2020-02-19 21:07:00 -0800

    fixed adding/finishing recipe... need to fix probabilities

--- shepherd/RecipeManager.py
+++ shepherd/RecipeManager.py
@@ -10,54 +10,63 @@ class RecipeManager():
     gold_recipes_completed = 0
     blue_recipes_completed = 0
 
-    def generate_recipes(self):
+    def generate_recipes():
         '''
         reset recipe list with 4 newly selected recipes
         both teams should have the same recipes
         '''
-        self.gold_recipes.clear()
-        self.blue_recipes.clear()
-        for recipe in self.master_recipes:
-            self.gold_recipes += list(recipe.values)
-            self.blue_recipes += list(recipe.values)
+        gold_recipes.clear()
+        blue_recipes.clear()
+        for recipe in master_recipes:
+            gold_recipes += list(recipe.values)
+            blue_recipes += list(recipe.values)
 
-    def next_recipe(self, team):
+    def finished_recipe(team):
         '''
         pop the recipe from the front of the list
 
         The recipe being popped is done
         '''
         if team == ALLIANCE_COLOR.BLUE:
-            if self.blue_recipes:
-                self.blue_recipes.pop(0)
-                self.blue_recipes_completed += 1
+            if blue_recipes:
+                blue_recipes.pop(0)
+                blue_recipes_completed += 1
         else:
-            if self.gold_recipes:
-                self.gold_recipes.pop(0)
-                self.gold_recipes_completed += 1
+            if gold_recipes:
+                gold_recipes.pop(0)
+                gold_recipes_completed += 1
 
-    def get_recipe(self, team):
+    def distribute_recipe():
+        '''
+        pop first recipe off master list and add it to both team's recipe list
+        '''
+        gold_recipes += master_recipes[0]
+        blue_recipes += master_recipes[0]
+        master_recipes.pop(0)
+    
+
+    def get_recipe(team):
         '''
         return first element of recipes
         '''
         if team == ALLIANCE_COLOR.BLUE:
-            if not self.blue_recipes:
+            if not blue_recipes:
                 return None
-            return self.blue_recipes[0]
+            return blue_recipes[0]
         else:
-            if not self.gold_recipes:
+            if not gold_recipes:
                 return None
-            return self.gold_recipes[0]
+            return gold_recipes[0]
 
-    def check_recipe(self, team, ingredients, num_cooked=0):
+    def check_recipe(team, ingredients, num_cooked=0):
         '''
         checks correctness of recipe
         '''
         recipes = []
         if team == ALLIANCE_COLOR.BLUE:
-            recipes = self.blue_recipes
+            recipes = blue_recipes
         else:
-            recipes = self.gold_recipes
+            recipes = gold_recipes
         if len(recipes) != len(ingredients):
             return False
         else:
@@ -66,12 +75,12 @@ class RecipeManager():
                     return False
         return True
 
-    def set_recipes(self, gold_recipes, blue_recipes):
+    def set_recipes(gold_recipes, blue_recipes):
         '''
         overwrite recipes
         '''
-        if gold_recipes != None:
-            self.gold_recipes = list(gold_recipes)
+        if gold_recipes is not None:
+            gold_recipes = list(gold_recipes)
 
-        if blue_recipes != None:
-            self.blue_recipes = list(blue_recipes)
+        if blue_recipes is not None:
+            blue_recipes = list(blue_recipes)

5c3300c3919297741ea45e062ed644d181ae267e
Oscar Chan <ochan2@berkeley.edu>
2020-02-29 15:01:38 -0800

    Randomized Team Recipe Generators Complete

--- shepherd/RecipeManager.py
+++ shepherd/RecipeManager.py
@@ -1,12 +1,27 @@
 import random
 from Utils import *
 
-master_recipes = [RECIPE_PROBABILITIES.EASY_RECIPE, RECIPE_PROBABILITIES.MEDIUM_RECIPE, \
+MASTER_RECIPES = [RECIPE_PROBABILITIES.EASY_RECIPE, RECIPE_PROBABILITIES.MEDIUM_RECIPE, \
     RECIPE_PROBABILITIES.HARD_RECIPE, RECIPE_PROBABILITIES.VERY_HARD_RECIPE]
+master_recipes_names = []
+master_recipes = []
 gold_recipes = []
 blue_recipes = []
 gold_recipes_completed = 0
 blue_recipes_completed = 0
+
+###### "Autograders" #####
+def generate_recipes_Autograder():
+    generate_recipes()
+    print(master_recipes_names)
+    print(master_recipes)
+
+def printAll():
+    print(master_recipes_names)
+    print(master_recipes)
+    print("Blue:", blue_recipes)
+    print("Gold:", gold_recipes)
+##########################
 
 def generate_recipes():
     '''
@@ -15,9 +30,40 @@ def generate_recipes():
     '''
     gold_recipes.clear()
     blue_recipes.clear()
-    for recipe in master_recipes:
-        gold_recipes.extend(list(recipe.values))
-        blue_recipes.extend(list(recipe.values))
+    master_recipes.clear()
+    master_recipes_names.clear()
+    recipe_probabilities = RECIPE_PROBABILITIES.RECIPE_PROBABILITIES
+    prob_keys = list(recipe_probabilities.keys())
+    for probKeys in prob_keys:
+        bounds = rngBoundaries(recipe_probabilities[probKeys])
+        randomSpoken = random.randint(1, 100)
+        for idx in range(len(bounds) - 1):
+            curr_i = len(bounds) - 1 - idx
+            if bounds[curr_i - 1] != bounds[curr_i] \
+                and bounds[curr_i - 1] < randomSpoken and randomSpoken <= bounds[curr_i]:
+                recipe_name, recipe_ingred = getRandomRecipes(MASTER_RECIPES[curr_i - 1])
+                master_recipes_names.append(recipe_name)
+                master_recipes.append(recipe_ingred)
+                break
+
+def rngBoundaries(probs):
+    overall = sum(probs)
+    bounds = [0]
+    for aProb in probs:
+        bounds.append(bounds[-1] + 100*aProb/overall)
+    return bounds
+
+def getRandomRecipes(recipe_level):
+    recipe_keys = list(recipe_level.keys())
+    rng_recipes = random.choice(recipe_keys)
+    return rng_recipes, list(recipe_level[rng_recipes])
+
+def allocateMinVals(lst):
+    probs = list(lst)
+    counter = probs[0]
+    for i in range(1, len(probs)):
+        probs[i] = counter + probs[i]
+    return probs
 
 def finished_recipe(team):
     '''
@@ -40,9 +86,12 @@ def distribute_recipe():
     '''
     pop first recipe off master list and add it to both team's recipe list
     '''
-    gold_recipes.extend(master_recipes[0])
-    blue_recipes.extend(master_recipes[0])
+    if not master_recipes:
+        return
+    gold_recipes.append(master_recipes[0])
+    blue_recipes.append(master_recipes[0])
     master_recipes.pop(0)
+    master_recipes_names.pop(0)
 
 def get_recipe(team):
     '''

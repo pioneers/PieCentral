edfc3aedf5a2297fd011f41c89620fb9beb5370f
Akshit Dewan <akshit@berkeley.edu>
2020-04-23 00:07:28 -0400

    test_online

--- shepherd/Unit_Tests/sheet_tests.py
+++ shepherd/Unit_Tests/sheet_tests.py
@@ -7,19 +7,13 @@ import unittest
 
 print(sheet.get_online_match(5))
 
-def get_online_match(match_number):
-	"""
-	hello!
-	>>> get_online_match(5)
-	{'b1name': 'El Cerrito', 'b1num': '12', 'b2name': 'Hercules', 'b2num': '9', 'g1name': 'LPS Richmond', 'g1num': '4', 'g2name': 'Middle College', 'g2num': '6'}
-	"""
-	sheet.get_online_match(match_number)
-
 class TestStringMethods(unittest.TestCase):
 
     def test_online(self):
-    	match_five = {'b1name': 'El Cerrito', 'b1num': '12', 'b2name': 'Hercules', 'b2num': '9', 'g1name': 'LPS Richmond', 'g1num': '4', 'g2name': 'Middle College', 'g2num': '6'}
-    	self.assertEqual(sheet.get_online_match(5), match_five)
+    	match_5 = {'b1name': 'El Cerrito', 'b1num': '12', 'b2name': 'Hercules', 'b2num': '9', 'g1name': 'LPS Richmond', 'g1num': '4', 'g2name': 'Middle College', 'g2num': '6'}
+    	match_24 = {'b1name': 'El Cerrito', 'b1num': '12', 'b2name': 'Albany', 'b2num': '1', 'g1name': 'REALM', 'g1num': '29', 'g2name': 'Encinal', 'g2num': '20'}
+    	self.assertEqual(sheet.get_online_match(5), match_5)
+    	self.assertEqual(sheet.get_online_match(24), match_24)
 
 if __name__ == '__main__':
     unittest.main()

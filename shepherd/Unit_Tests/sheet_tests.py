3ec1d7e0e2d808dca2429f73f68c5483d44b04dc
Matthew Harrigan <betterthanbossfiredog5@gmail.com>
2020-04-22 20:18:23 -0700

    import plz work

---
+++ shepherd/Unit_Tests/sheet_tests.py
@@ -0,0 +1,7 @@
+import sys
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = dir_path.replace('/Unit_Tests', '')
+dir_path = dir_path.replace('\\Unit_Tests', '')
+sys.path.append(dir_path)
+import Sheet.py as sheet
